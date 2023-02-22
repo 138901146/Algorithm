7f6d99fbb66cf4502985842b99f7080cea8e00db
138901146 <126007127+138901146@users.noreply.github.com>
2023-02-23 01:46:41 +0900

    [Bronze I] Title: 뒤집힌 덧셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int Rev(int X)
+{
+	int reversed=0;
+
+	while(X>0)
+	{
+		reversed*=10;
+		reversed+=X%10;
+		X/=10;
+	}
+
+	return reversed;
+}
+
+int main(void)
+{
+	int X, Y;
+
+	scanf("%d %d", &X, &Y);
+
+	X=Rev(X);
+	Y=Rev(Y);
+
+	printf("%d\n", Rev(X+Y));
+
+	return 0;
+}
