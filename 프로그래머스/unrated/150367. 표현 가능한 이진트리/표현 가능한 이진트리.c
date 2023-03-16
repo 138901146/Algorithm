fe8909b6dfd50ab4f7ee5fb7fd235c63caa6b0da
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-03-16 15:45:22 +0900

    [unrated] Title: 표현 가능한 이진트리, Time: 3.21 ms, Memory: 4.79 MB -BaekjoonHub

--- 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.c
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.c
@@ -6,7 +6,7 @@
 int* solution(long long numbers[], size_t numbers_len) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int* answer = (int*)malloc(numbers_len*sizeof(int));
-    int bit[64], left[64], right[64], level[64], queue[64];
+    int bit[64], left[64], right[64], level[64];
 
     for(int i=1;i<64;i++)
     {
@@ -18,18 +18,18 @@ int* solution(long long numbers[], size_t numbers_len) {
         }
         level[i]=count;
     }
-    
+
     for(int i=0;i<numbers_len;i++)
     {
         answer[i]=1;
         long long temp=numbers[i];
-        int size=0, front=0, rear=0, count=0;
+        int size=0;
         while(temp>0)
         {
             size++;
             temp>>=1;
         }
-//        size++;
+
         temp=0;
         while(size>=(1<<temp))
             temp++;
@@ -40,39 +40,19 @@ int* solution(long long numbers[], size_t numbers_len) {
         {
             left[j]=right[j]=0;
             bit[j]=temp&1;
-            count+=bit[j];
         }
-        
+
         for(int j=2;j<=size;j++)
             left[j]=bit[j-1]==1?0:left[j-1]+1;
         for(int j=size-1;j>0;j--)
             right[j]=bit[j+1]==1?0:right[j+1]+1;
 
-//        for(int j=1;j<=size;j++)
-//            if(bit[j]==0 && (left[j]<level[j] || right[j]<level[j]))
-//            {
-//                answer[i]=0;
-//                break;
-//            }
-        
-        if(bit[(size+1)/2]==1)
-            queue[rear++]=(size+1)/2;
-        while(front<rear)
-        {
-            if(level[queue[front]]>0)
+        for(int j=1;j<=size;j++)
+            if(bit[j]==0 && (left[j]<(1<<level[j])-1 || right[j]<(1<<level[j])-1))
             {
-                if(bit[queue[front]+(1<<(level[queue[front]]-1))]==1)
-                    queue[rear++]=queue[front]+(1<<(level[queue[front]]-1));
-                if(bit[queue[front]-(1<<(level[queue[front]])-1)]==1)
-                    queue[rear++]=queue[front]-(1<<(level[queue[front]]-1));
+                answer[i]=0;
+                break;
             }
-            front++;
-        }
-        if(count!=rear)
-        {
-            printf("%lld: %d %d ", numbers[i], count, rear);
-            answer[i]=0;
-        }
     }
 
     return answer;
