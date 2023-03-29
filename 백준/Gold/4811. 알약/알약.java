4e8824375a8c64fb1fb1fdcbc06a7388396ae114
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-03-29 11:35:26 +0900

    [Gold V] Title: 알약, Time: 116 ms, Memory: 12364 KB -BaekjoonHub

---
+++ 백준/Gold/4811. 알약/알약.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		int N;
+		long[][] method=new long[31][31];
+		
+		for(int i=1;i<31;i++) {
+			method[i][0]=1;
+			for(int j=1;j<=i;j++)
+				method[i][j]=method[i-1][j]+method[i][j-1];
+		}
+		
+		N=Integer.parseInt(br.readLine());
+		while(N>0) {
+			System.out.println(method[N][N]);
+			N=Integer.parseInt(br.readLine());
+		}
+	}
+}
