064320ed6da6c41dd87764bdaf994fa8064a5c01
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-04-04 09:49:33 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 236 ms, Memory: 5020 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+	int length=0, N, A, *LIS=NULL;
+
+	scanf("%d", &N);
+	LIS=(int *)malloc(N*sizeof(int));
+
+	for(int i=0;i<N;i++)
+	{
+		int left=0, right=length, mid;
+		LIS[i]=10000000;
+		scanf("%d", &A);
+
+		while(left<right)
+		{
+			mid=(left+right)/2;
+			if(LIS[mid]>A)
+				right=mid-1;
+			else if(LIS[mid]<A)
+				left=mid+1;
+			else
+				break;
+		}
+		mid=(left+right)/2;
+
+		if(A<=LIS[mid])
+			LIS[mid]=A;
+		else
+			LIS[mid+1]=A;
+		length+=mid==length;
+	}
+
+	printf("%d\n", length);
+	free(LIS);
+	return 0;
+}
