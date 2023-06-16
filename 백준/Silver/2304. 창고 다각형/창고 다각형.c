377d539f2e7e4ac6e24b79299e3ce9dbe0f8e3f5
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-06-16 16:33:17 +0900

    [Silver II] Title: 창고 다각형, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.c
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.c
@@ -2,36 +2,26 @@
 
 int main(void)
 {
-	int N, pillar[1001]={0, }, *left=NULL, *right=NULL, L, H, left_current=1, right_current=1, area=0;
+	int N, pillar[1002]={0, }, prefix[1002]={0, }, suffix[1002]={0, }, sum=0, L, H, left=1001, right=0;
 
 	scanf("%d", &N);
-	left=(int *)calloc(N+1,sizeof(int));
-	right=(int *)calloc(N+1,sizeof(int));
 
-	for(int n=0;n<N;n++)
+	for(int i=0;i<N;i++)
 	{
 		scanf("%d%d", &L, &H);
 		pillar[L]=H;
+		left=L<left?L:left;
+		right=L>right?L:right;
 	}
 
-	left[0]=0;
-	for(int n=0;n<1001;n++)
-		if(pillar[n]>pillar[left[left_current-1]])
-			left[left_current++]=n;
+	for(int i=left;i<=right;i++)
+		prefix[i]=pillar[i]>prefix[i-1]?pillar[i]:prefix[i-1];
+	for(int i=right;i>=left;i--)
+		suffix[i]=pillar[i]>suffix[i+1]?pillar[i]:suffix[i+1];
 
-	right[0]=1000;
-	for(int n=1000;n>=0;n--)
-		if(pillar[n]>pillar[right[right_current-1]])
-			right[right_current++]=n;
+	for(int i=left;i<=right;i++)
+		sum+=prefix[i]<suffix[i]?prefix[i]:suffix[i];
 
-	for(int i=1;i<left_current;i++)
-		area+=(left[i]-left[i-1])*pillar[left[i-1]];
-	for(int i=1;i<right_current;i++)
-		area+=(right[i-1]-right[i])*pillar[right[i-1]];
-	area+=(right[right_current-1]-left[left_current-1]+1)*pillar[left[left_current-1]];
-
-	printf("%d\n", area);
-	free(right);
-	free(left);
+	printf("%d\n", sum);
 	return 0;
 }
