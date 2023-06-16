a362a4c53554b663810f7d17ef5f0c06aedc5f73
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-06-17 00:51:47 +0900

    [Gold III] Title: 나눌 수 있는 부분 수열, Time: 188 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Gold/3673. 나눌 수 있는 부분 수열/나눌 수 있는 부분 수열.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<malloc.h>
+
+int main(void)
+{
+	int C;
+
+	scanf("%d", &C);
+
+	for(int c=0;c<C;c++)
+	{
+		int d, n;
+		long long *arr=NULL, count=0, *remain=NULL;
+
+		scanf("%d%d", &d, &n);
+		arr=(long long *)calloc(n+1,sizeof(long long));
+		remain=(long long *)calloc(d,sizeof(long long));
+		remain[0]=1;
+
+		for(int i=1;i<=n;i++)
+		{
+			scanf("%lld", &arr[i]);
+			arr[i]+=arr[i-1];
+			count+=remain[arr[i]%d];
+			remain[arr[i]%d]++;
+		}
+
+		printf("%lld\n", count);
+		free(arr);
+		free(remain);
+	}
+
+	return 0;
+}
