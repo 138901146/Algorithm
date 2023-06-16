13c2124a3f07d6bb7f8724ab34ba2990f08e4216
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-06-16 16:59:52 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 196 ms, Memory: 5208 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.c
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.c
@@ -1,40 +1,32 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<malloc.h>
 
 int main(void)
 {
-	int N, M, **arr=NULL, **accumulate=NULL;
+	int N, M, **sum=NULL, x1, y1, x2, y2;
 
 	scanf("%d%d", &N, &M);
-	arr=(int **)malloc((N+1)*sizeof(int *));
-	accumulate=(int **)malloc((N+1)*sizeof(int *));
-	for(int n=0;n<=N;n++)
-	{
-		arr[n]=(int *)calloc(N+1,sizeof(int));
-		accumulate[n]=(int *)calloc(N+1,sizeof(int));
-	}
+	sum=(int **)malloc((N+1)*sizeof(int *));
 
+	sum[0]=(int *)calloc(N+1,sizeof(int));
 	for(int i=1;i<=N;i++)
+	{
+		sum[i]=(int *)calloc(N+1,sizeof(int));
 		for(int j=1;j<=N;j++)
 		{
-			scanf("%d", &arr[i][j]);
-			accumulate[i][j]=arr[i][j]+accumulate[i-1][j]+accumulate[i][j-1]-accumulate[i-1][j-1];
+			scanf("%d", &sum[i][j]);
+			sum[i][j]=sum[i][j]+sum[i-1][j]+sum[i][j-1]-sum[i-1][j-1];
 		}
+	}
 
 	for(int m=0;m<M;m++)
 	{
-		int x1, x2, y1, y2;
-
 		scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
-		printf("%d\n", accumulate[x2][y2]-accumulate[x2][y1-1]-accumulate[x1-1][y2]+accumulate[x1-1][y1-1]);
+		printf("%d\n", sum[x2][y2]-sum[x2][y1-1]-sum[x1-1][y2]+sum[x1-1][y1-1]);
 	}
 
 	for(int n=0;n<=N;n++)
-	{
-		free(arr[n]);
-		free(accumulate[n]);
-	}
-	free(arr);
-	free(accumulate);
+		free(sum[n]);
+	free(sum);
 	return 0;
 }
