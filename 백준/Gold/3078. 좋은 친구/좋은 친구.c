46def1efdcc97af1ace4b19e6e259625a311164e
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-06-21 04:32:52 +0900

    [Gold IV] Title: 좋은 친구, Time: 84 ms, Memory: 2284 KB -BaekjoonHub

---
+++ 백준/Gold/3078. 좋은 친구/좋은 친구.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<string.h>
+
+int main(void)
+{
+	int N, K, friend[21]={0, }, *length=NULL;
+	long long sum=0;
+	char name[21]={'\0', };
+
+	scanf("%d%d", &N, &K);
+	length=(int *)malloc(N*sizeof(int));
+
+	for(int n=0;n<N;n++)
+	{
+		scanf("%s", name);
+		length[n]=strlen(name);
+
+		if(n>K)
+			friend[length[n-K-1]]--;
+
+		sum+=friend[length[n]];
+		friend[length[n]]++;
+	}
+
+	printf("%lld\n", sum);
+	free(length);
+	return 0;
+}
