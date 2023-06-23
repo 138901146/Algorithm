51ee63a9cd370a340186861135b3b9bc0bfdc555
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-06-23 19:58:30 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.c
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.c
@@ -2,35 +2,15 @@
 
 int main(void)
 {
-	int dwarf[9];
+	int dwarf[9], difference=-100, fake[2]={-1,-1};
 
 	for(int i=0;i<9;i++)
-		scanf("%d", &dwarf[i]);
-
-	for(int i=0;i<8;i++)
 	{
-		int sum=0;
-		for(int j=i+1;j<9;j++)
-		{
-			sum=0;
-
-			for(int d=0;d<9;d++)
-				if(d==i || d==j)
-					continue;
-				else
-					sum+=dwarf[d];
-			if(sum==100)
-			{
-				dwarf[i]=dwarf[j]=0;
-				break;
-			}
-		}
-
-		if(sum==100)
-			break;
+		scanf("%d", &dwarf[i]);
+		difference+=dwarf[i];
 	}
 
-	for(int i=0;i<8;i++)
+	for(int i=0;i<9;i++)
 		for(int j=i+1;j<9;j++)
 			if(dwarf[i]>dwarf[j])
 			{
@@ -39,8 +19,18 @@ int main(void)
 				dwarf[j]=temp;
 			}
 
-	for(int i=2;i<9;i++)
-		printf("%d\n", dwarf[i]);
+	for(int i=0;i<9&&fake[0]==-1;i++)
+		for(int j=i+1;j<9;j++)
+			if(dwarf[i]+dwarf[j]==difference)
+			{
+				fake[0]=i;
+				fake[1]=j;
+				break;
+			}
+
+	for(int i=0;i<9;i++)
+		if(i!=fake[0] && i!=fake[1])
+			printf("%d\n", dwarf[i]);
 
 	return 0;
 }
