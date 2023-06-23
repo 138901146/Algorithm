5ce7046a251f80edb8fe25723345233651589cd4
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-06-23 19:51:38 +0900

    [Silver IV] Title: 수들의 합 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.c
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.c
@@ -1,22 +1,38 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<malloc.h>
 
 int main(void)
 {
-	int N, *A=NULL, M, count=0;
+	int N, M, *A=NULL, count=0;
 
-	scanf("%d %d", &N, &M);
-	A=(int *)malloc(N*sizeof(int));
-	for(int i=0;i<N;i++)
-		scanf("%d", &A[i]);
+	scanf("%d%d", &N, &M);
+	A=(int *)malloc((N+1)*sizeof(int));
+	A[0]=0;
 
-	for(int i=0;i<N;i++)
+	for(int i=1;i<=N;i++)
 	{
-		int sum=0;
+		scanf("%d", &A[i]);
+		A[i]+=A[i-1];
+	}
 
-		for(int j=i;j<N&&sum<M;j++)
-			sum+=A[j];
-		count+=sum==M;
+	for(int i=N;i>0&&A[i]>=M;i--)
+	{
+		int left=0, right=i-1, mid;
+
+		while(left<right)
+		{
+			mid=(left+right)>>1;
+
+			if(A[i]-A[mid]==M)
+				break;
+			else if(A[i]-A[mid]<M)
+				right=mid-1;
+			else
+				left=mid+1;
+		}
+
+		mid=(left+right)>>1;
+		count+=A[i]-A[mid]==M;
 	}
 
 	printf("%d\n", count);
