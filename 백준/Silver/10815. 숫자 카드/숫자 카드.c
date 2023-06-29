6ed4c91d5af54796aa34b5e368ef3d1a22a818b7
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-06-29 22:28:53 +0900

    [Silver V] Title: 숫자 카드, Time: 252 ms, Memory: 3724 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.c
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.c
@@ -1,35 +1,26 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
+#include<malloc.h>
 
 int main(void)
 {
-	int N, M, number;
-	bool *plus=NULL, *minus=NULL;
+	int N, M, *card=(int *)calloc(666667,sizeof(int)), num;
 
-	plus=(bool *)calloc(10000001,sizeof(bool));
-	minus=(bool *)calloc(10000000,sizeof(bool));
 	scanf("%d", &N);
-
 	for(int n=0;n<N;n++)
 	{
-		scanf("%d", &number);
-
-		if(number<0)
-			minus[-number-1]=true;
-		else
-			plus[number]=true;
+		scanf("%d", &num);
+		num+=10000000;
+		card[num/30]|=(1<<(num%30));
 	}
 
 	scanf("%d", &M);
 	for(int m=0;m<M;m++)
 	{
-		scanf("%d", &number);
-		printf("%d ", number<0?minus[-number-1]:plus[number]);
+		scanf("%d", &num);
+		num+=10000000;
+		printf("%d ", (card[num/30]&(1<<(num%30)))?1:0);
 	}
 
-	printf("\n");
-	free(minus);
-	free(plus);
+	free(card);
 	return 0;
 }
