0714a82d02eebf2a8b0898c465af4090d83feccf
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-06-30 03:43:35 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 392 ms, Memory: 79240 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.c
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.c
@@ -1,25 +1,24 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<malloc.h>
 
 int main(void)
 {
-	int N, M, *num=calloc(20000001,sizeof(int)), card;
+	int N, M, *card=(int *)calloc(20000001,sizeof(int)), num;
 
 	scanf("%d", &N);
 	for(int n=0;n<N;n++)
 	{
-		scanf("%d", &card);
-		num[card+10000000]++;
+		scanf("%d", &num);
+		card[num+10000000]++;
 	}
 
 	scanf("%d", &M);
 	for(int m=0;m<M;m++)
 	{
-		scanf("%d", &card);
-		printf("%d ", num[card+10000000]);
+		scanf("%d", &num);
+		printf("%d ", card[num+10000000]);
 	}
 
-	printf("\n");
-	free(num);
+	free(card);
 	return 0;
 }
