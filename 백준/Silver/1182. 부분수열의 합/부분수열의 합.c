a4449769ec1ea0c3bfd58a615da72680e99e08f7
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-07-07 00:00:00 +0900

    [Silver II] Title: 부분수열의 합, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.c
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.c
@@ -1,24 +1,27 @@
 #include<stdio.h>
-#include<stdlib.h>
 
-int backtrack(int *arr,int current,int N,int S,int sum,int count)
+int arr[20], count=0, N, S;
+
+void partial_array(int index,int sum,int used)
 {
-	if(current==N)
-		return sum==S&&count>0;
-	return backtrack(arr,current+1,N,S,sum+arr[current],count+1)+backtrack(arr,current+1,N,S,sum,count);
+	if(index==N)
+	{
+		count+=sum==S&&used>0;
+		return;
+	}
+	partial_array(index+1,sum+arr[index],used+1);
+	partial_array(index+1,sum,used);
 }
 
 int main(void)
 {
-	int N, *arr=NULL, S;
-
 	scanf("%d%d", &N, &S);
-	arr=(int *)malloc(N*sizeof(int));
 
 	for(int i=0;i<N;i++)
 		scanf("%d", &arr[i]);
 
-	printf("%d\n", backtrack(arr,0,N,S,0,0));
-	free(arr);
+	partial_array(0,0,0);
+
+	printf("%d\n", count);
 	return 0;
 }
