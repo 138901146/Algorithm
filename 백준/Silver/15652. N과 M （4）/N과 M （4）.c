49d6182967245340493f02744d42631e30c53331
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-07-06 22:54:12 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.c
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.c
@@ -1,32 +1,28 @@
 #include<stdio.h>
-#include<stdlib.h>
 
-void N_M(int *A,int N,int M,int current)
+int arr[8], N, M;
+
+void N_M(int x,int y)
 {
-	if(current==M+1)
+	if(y==M)
 	{
-		for(int m=1;m<=M;m++)
-			printf("%d ", A[m]);
+		for(int i=0;i<M;i++)
+			printf("%d ", arr[i]);
 		printf("\n");
+		return;
 	}
-	else
-		for(int n=A[current-1];n<=N;n++)
-		{
-			A[current]=n;
-			N_M(A,N,M,current+1);
-		}
+
+	for(int i=x;i<=N;i++)
+	{
+		arr[y]=i;
+		N_M(i,y+1);
+	}
 }
 
 int main(void)
 {
-	int N, M, *A=NULL;
+	scanf("%d%d", &N, &M);
 
-	scanf("%d %d", &N, &M);
-	A=(int *)malloc((N+1)*sizeof(int));
-
-	A[0]=1;
-	N_M(A,N,M,1);
-
-	free(A);
+	N_M(1,0);
 	return 0;
 }
