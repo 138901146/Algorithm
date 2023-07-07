3e86b25913cb58d4b42354acf8a2134ec7d81468
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-07-08 08:25:54 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.c
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.c
@@ -1,41 +1,29 @@
 #include<stdio.h>
-#include<stdlib.h>
-
-int N, S[10], B[10];
-
-int food(int current,int set)
-{
-	if(current==N)
-	{
-		if(set>0)
-		{
-			int s=1, b=0;
-
-			for(int n=0;n<N;n++)
-				if(set & 1<<n)
-				{
-					s*=S[n];
-					b+=B[n];
-				}
-
-			return s-b>=0?s-b:b-s;
-		}
-		return 1000000000;
-	}
-
-	int used=food(current+1,set|1<<current);
-	int unused=food(current+1,set);
-
-	return used<unused?used:unused;
-}
 
 int main(void)
 {
+	int N, bitter[10], sour[10], min=2000000000;
+
 	scanf("%d", &N);
 
 	for(int n=0;n<N;n++)
-		scanf("%d%d", &S[n], &B[n]);
+		scanf("%d%d", &sour[n], &bitter[n]);
 
-	printf("%d\n", food(0,0));
+	for(int bit=1;bit<(1<<N);bit++)
+	{
+		int sum=0, mul=1, diff;
+
+		for(int n=0;n<N;n++)
+			if(bit&(1<<n))
+			{
+				sum+=bitter[n];
+				mul*=sour[n];
+			}
+
+		diff=sum<mul?mul-sum:sum-mul;
+		min=diff<min?diff:min;
+	}
+
+	printf("%d\n", min);
 	return 0;
 }
