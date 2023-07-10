c5d09a3f5126a260c6d389f96e572c19317d51e6
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-07-11 02:50:03 +0900

    [Gold V] Title: 리모컨, Time: 12 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.c
+++ 백준/Gold/1107. 리모컨/리모컨.c
@@ -1,63 +1,60 @@
 #include<stdio.h>
 #include<stdbool.h>
 
-int abs(int N)
+int abs(int x)
 {
-	return N>0?N:-N;
+	return x<0?-x:x;
 }
 
 int main(void)
 {
-	int N, M, num, hundred, best=999999999, count=0;
-	bool normal[10];
-
-	for(int i=0;i<10;i++)
-		normal[i]=true;
+	int N, M, number, min;
+	bool broken[10]={false, };
 
 	scanf("%d", &N);
 	scanf("%d", &M);
 	for(int m=0;m<M;m++)
 	{
-		scanf("%d", &num);
-		normal[num]=false;
+		scanf("%d", &number);
+		broken[number]=true;
 	}
 
-	hundred=abs(N-100);
+	min=abs(N-100);
 
-	if(hundred<2 || M==10)
+	if(min<2 || M==10)
 	{
-		printf("%d\n", hundred);
+		printf("%d\n", min);
 		return 0;
 	}
 
-	for(int n=0;n<1000000;n++)
+	for(int i=0;i<10;i++)
+		if(!broken[i])
+		{
+			min=i+1+N<min?i+1+N:min;
+			break;
+		}
+
+	for(int n=1;n<1000000;n++)
 	{
 		bool able=true;
-		count=0;
-		num=n;
+		int count=0;
+		number=n;
 
-		if(num==0)
-			for(int i=0;i<10;i++)
-				if(normal[i])
-				{
-					count=i+1;
-					break;
-				}
-		while(num>0)
+		while(number>0)
 		{
-			if(!normal[num%10])
+			if(broken[number%10])
 			{
 				able=false;
 				break;
 			}
 			count++;
-			num/=10;
+			number/=10;
 		}
 
 		if(able)
-			best=count+abs(N-n)>best?best:count+abs(N-n);
+			min=count+abs(N-n)>min?min:count+abs(N-n);
 	}
 
-	printf("%d\n", best>hundred?hundred:best);
+	printf("%d\n", min);
 	return 0;
 }
