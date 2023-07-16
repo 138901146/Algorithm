c291b52618180f8f374aec9f1965b67cb057d9db
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-07-16 14:42:11 +0900

    [Gold I] Title: 택배, Time: 12 ms, Memory: 1360 KB -BaekjoonHub

--- 백준/Gold/8980. 택배/README.md
+++ 백준/Gold/8980. 택배/README.md
@@ -1,10 +1,10 @@
-# [Gold II] 택배 - 8980 
+# [Gold I] 택배 - 8980 
 
 [문제 링크](https://www.acmicpc.net/problem/8980) 
 
 ### 성능 요약
 
-메모리: 1116 KB, 시간: 0 ms
+메모리: 1360 KB, 시간: 12 ms
 
 ### 분류
 
--- 백준/Gold/8980. 택배/택배.c
+++ 백준/Gold/8980. 택배/택배.c
@@ -14,31 +14,35 @@ int compare(const void *x,const void *y)
 
 int main(void)
 {
-	int N, C, M, answer=0;
+	int N, C, M, sum=0, load, max, *state=NULL;
 	boxinfo *delivery=NULL;
 
 	scanf("%d%d", &N, &C);
 	scanf("%d", &M);
 
+	state=(int *)calloc(N+1,sizeof(int));
 	delivery=(boxinfo *)malloc(M*sizeof(boxinfo));
 	for(int m=0;m<M;m++)
 		scanf("%d%d%d", &delivery[m].from, &delivery[m].to, &delivery[m].count);
 
 	qsort((void *)delivery,(size_t)M,sizeof(boxinfo),compare);
 
-	for(int c=0;c<C;c++)
+	for(int m=0;m<M;m++)
 	{
-		int last=-1;
-		for(int m=0;m<M;m++)
-			if(delivery[m].from>=last && delivery[m].count>0)
-			{
-				last=delivery[m].to;
-				delivery[m].count--;
-				answer++;
-			}
+		max=0;
+
+		for(int i=delivery[m].from;i<delivery[m].to;i++)
+			max=max>state[i]?max:state[i];
+
+		load=C-max<delivery[m].count?C-max:delivery[m].count;
+		sum+=load;
+
+		for(int i=delivery[m].from;i<delivery[m].to;i++)
+			state[i]+=load;
 	}
 
-	printf("%d\n", answer);
+	printf("%d\n", sum);
+	free(state);
 	free(delivery);
 	return 0;
 }
