592ac91633baa59912c8562a9c348fb5333a6ad6
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-07-17 06:37:36 +0900

    [Platinum V] Title: 달려라 홍준, Time: 524 ms, Memory: 13220 KB -BaekjoonHub

---
+++ 백준/Platinum/1306. 달려라 홍준/달려라 홍준.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<malloc.h>
+
+void init(int *arr,int *tree,int node,int start,int end)
+{
+	if(start==end)
+		tree[node]=arr[start];
+	else
+	{
+		init(arr,tree,node<<1,start,(start+end)>>1);
+		init(arr,tree,node<<1|1,((start+end)>>1)+1,end);
+		tree[node]=tree[node<<1]>tree[node<<1|1]?tree[node<<1]:tree[node<<1|1];
+	}
+}
+
+int get(int *tree,int node,int start,int end,int left,int right)
+{
+	if(left>end || right<start)
+		return 0;
+	else if(left<=start && end<=right)
+		return tree[node];
+	else
+	{
+		int left_child=get(tree,node<<1,start,(start+end)>>1,left,right), right_child=get(tree,node<<1|1,((start+end)>>1)+1,end,left,right);
+		return left_child>right_child?left_child:right_child;
+	}
+}
+
+int main(void)
+{
+	int N, M, size=0, *arr=NULL, *tree=NULL;
+
+	scanf("%d%d", &N, &M);
+	while((1<<size)<N)
+		size++;
+	size++;
+	arr=(int *)malloc(N*sizeof(int));
+	tree=(int *)malloc((1<<size)*sizeof(int));
+
+	for(int n=0;n<N;n++)
+		scanf("%d", &arr[n]);
+	init(arr,tree,1,0,N-1);
+
+	for(int i=M-1;i<N-M+1;i++)
+		printf("%d ", get(tree,1,0,N-1,i-M+1,i+M-1));
+	printf("\n");
+	free(arr);
+	free(tree);
+	return 0;
+}
