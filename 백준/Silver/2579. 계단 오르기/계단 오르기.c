4768561e3f55a326725bd67a464beba198b287f6
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-07-20 22:52:11 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.c
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.c
@@ -1,35 +1,18 @@
 #include<stdio.h>
-#include<stdlib.h>
-
-int max(int x,int y)
-{
-	return x>y?x:y;
-}
 
 int main(void)
 {
-	int *stairs=NULL, num, **score=NULL;
+	int N, stair, max[303][2]={0, };
 
-	scanf("%d", &num);
-	stairs=(int *)calloc(num+1,sizeof(int));
-	score=(int **)malloc((num+1)*sizeof(int *));
-	for(int i=0;i<=num;i++)
-		score[i]=(int *)calloc(2,sizeof(int));
-
-	for(int i=1;i<=num;i++)
-		scanf("%d", &stairs[i]);
-
-	score[1][0]=stairs[1];
-	for(int i=2;i<=num;i++)
+	scanf("%d", &N);
+	for(int n=1;n<=N;n++)
 	{
-		score[i][0]=stairs[i]+max(score[i-2][0],score[i-2][1]);
-		score[i][1]=stairs[i]+score[i-1][0];
+		scanf("%d", &stair);
+		int local=max[n][0]>max[n][1]?max[n][0]:max[n][1];
+		max[n+2][0]=stair+local>max[n+2][0]?stair+local:max[n+2][0];
+		max[n+1][1]=stair+max[n][0]>max[n+1][1]?stair+max[n][0]:max[n+1][1];
 	}
 
-	printf("%d\n", max(score[num][0],score[num][1]));
-	for(int i=0;i<=num;i++)
-		free(score[i]);
-	free(score);
-	free(stairs);
+	printf("%d\n", max[N][0]<max[N][1]?stair+max[N][1]:stair+max[N][0]);
 	return 0;
 }
