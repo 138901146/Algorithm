ffce2896aab40c4b99ff37898fb14ad856246e3c
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-08-03 22:52:14 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 68 ms, Memory: 13448 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.c
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.c
@@ -1,74 +1,62 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<malloc.h>
+
+int *parent=NULL, **child=NULL, *child_count=NULL;
+
+void find_parent(int current)
+{
+	for(int i=0;i<child_count[current];i++)
+		if(child[current][i]!=1 && !parent[child[current][i]])
+		{
+			parent[child[current][i]]=current;
+			find_parent(child[current][i]);
+		}
+}
 
 int main(void)
 {
-	int N, **connected=NULL, *connected_count=NULL, **tree=NULL, *queue=NULL, *parent=NULL, front=0, rear=0;
+	int N, **edge=NULL;
 
 	scanf("%d", &N);
-	connected=(int **)malloc((N-1)*sizeof(int *));
-	for(int n=0;n<N-1;n++)
-		connected[n]=(int *)malloc(2*sizeof(int));
-	connected_count=(int *)calloc(N+1,sizeof(int));
+	edge=(int **)malloc((N-1)*sizeof(int *));
 	parent=(int *)calloc(N+1,sizeof(int));
-	tree=(int **)malloc((N+1)*sizeof(int *));
-	queue=(int *)malloc(N*sizeof(int));
-	for(int i=0;i<N-1;i++)
-	{
-		scanf("%d%d", &connected[i][0], &connected[i][1]);
-		if(connected[i][0]==1)
-		{
-			parent[connected[i][1]]=1;
-			queue[rear++]=connected[i][1];
-		}
-		else if(connected[i][1]==1)
-		{
-			parent[connected[i][0]]=1;
-			queue[rear++]=connected[i][0];
-		}
-		else
-		{
-			connected_count[connected[i][0]]++;
-			connected_count[connected[i][1]]++;
-		}
-	}
+	child_count=(int *)calloc(N+1,sizeof(int));
+	child=(int **)malloc((N+1)*sizeof(int *));
 
-	for(int i=1;i<=N;i++)
-		tree[i]=(int *)calloc(connected_count[i],sizeof(int));
-
-	for(int i=0;i<=N;i++)
-		connected_count[i]=0;
-
-	for(int i=0;i<N-1;i++)
-	{
-		if(connected[i][0]!=1 && connected[i][1]!=1)
-		{
-			tree[connected[i][0]][connected_count[connected[i][0]]++]=connected[i][1];
-			tree[connected[i][1]][connected_count[connected[i][1]]++]=connected[i][0];
-		}
-	}
-
-	while(front<rear)
-	{
-		for(int i=0;i<connected_count[queue[front]];i++)
-			if(parent[tree[queue[front]][i]]==0)
-			{
-				parent[tree[queue[front]][i]]=queue[front];
-				queue[rear++]=tree[queue[front]][i];
-			}
-		front++;
-	}
-
-	for(int i=2;i<=N;i++)
-		printf("%d\n", parent[i]);
-	for(int i=1;i<=N;i++)
-		free(tree[i]);
-	free(tree);
-	free(queue);
-	free(parent);
-	free(connected_count);
 	for(int n=0;n<N-1;n++)
-		free(connected[n]);
-	free(connected);
+	{
+		edge[n]=(int *)malloc(2*sizeof(int));
+		for(int i=0;i<2;i++)
+		{
+			scanf("%d", &edge[n][i]);
+			child_count[edge[n][i]]++;
+		}
+	}
+
+	for(int n=1;n<=N;n++)
+	{
+		child[n]=(int *)malloc(child_count[n]*sizeof(int));
+		child_count[n]=0;
+	}
+
+	for(int n=0;n<N-1;n++)
+	{
+		child[edge[n][0]][child_count[edge[n][0]]++]=edge[n][1];
+		child[edge[n][1]][child_count[edge[n][1]]++]=edge[n][0];
+		free(edge[n]);
+	}
+	free(edge);
+
+	find_parent(1);
+
+	free(child[1]);
+	for(int n=2;n<=N;n++)
+	{
+		printf("%d\n", parent[n]);
+		free(child[n]);
+	}
+	free(child);
+	free(parent);
+	free(child_count);
 	return 0;
 }
