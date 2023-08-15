768dabef510697877967382468a6e6c930c6ec2e
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-08-15 18:27:24 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 16 ms, Memory: 1352 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.c
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.c
@@ -1,36 +1,24 @@
 #include<stdio.h>
-#include<stdlib.h>
 
 int main(void)
 {
-	int N, M, **arr=NULL, K;
+	int N, M, arr[301][301]={0, }, K, i, j, x, y;
 
-	scanf("%d %d", &N, &M);
-	arr=(int **)malloc(N*sizeof(int *));
-	for(int n=0;n<N;n++)
-		arr[n]=(int *)malloc(M*sizeof(int));
+	scanf("%d%d", &N, &M);
 
-	for(int n=0;n<N;n++)
-		for(int m=0;m<M;m++)
+	for(int n=1;n<=N;n++)
+		for(int m=1;m<=M;m++)
+		{
 			scanf("%d", &arr[n][m]);
+			arr[n][m]+=arr[n-1][m]+arr[n][m-1]-arr[n-1][m-1];
+		}
 
 	scanf("%d", &K);
-
-	for(int k=0;k<K;k++)
+	while(K--)
 	{
-		int i, j, x, y, sum=0;
-
-		scanf("%d %d %d %d", &i, &j, &x, &y);
-
-		for(int n=i-1;n<x;n++)
-			for(int m=j-1;m<y;m++)
-			sum+=arr[n][m];
-
-		printf("%d\n", sum);
+		scanf("%d%d%d%d", &i, &j, &x, &y);
+		printf("%d\n", arr[x][y]-arr[x][j-1]-arr[i-1][y]+arr[i-1][j-1]);
 	}
 
-	for(int n=0;n<N;n++)
-		free(arr[n]);
-	free(arr);
 	return 0;
 }
