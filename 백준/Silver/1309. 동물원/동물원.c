6782b371de605072b6e8ed5e813cb28e342379b6
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-08-31 12:37:21 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.c
+++ 백준/Silver/1309. 동물원/동물원.c
@@ -1,28 +1,20 @@
 #include<stdio.h>
-#include<malloc.h>
 
 int main(void)
 {
-	int N, *cage=NULL, *until=NULL;
+	int N, cage[3]={1,0,0}, until[3]={3,1,1};
 
 	scanf("%d", &N);
-	cage=(int *)malloc(N*sizeof(int));
-	until=(int *)malloc(N*sizeof(int));
-
-	cage[0]=1;
-	until[0]=3;
 
 	for(int n=1;n<N;n++)
 	{
-		cage[n]=cage[n-1]+(n>1?until[n-2]:1);
-		until[n]=until[n-1]+(cage[n]<<1);
+		cage[n%3]=cage[(n+2)%3]+until[(n+1)%3];
+		until[n%3]=until[(n+2)%3]+(cage[n%3]<<1);
 
-		cage[n]%=9901;
-		until[n]%=9901;
+		cage[n%3]%=9901;
+		until[n%3]%=9901;
 	}
 
-	printf("%d\n", until[N-1]);
-	free(until);
-	free(cage);
+	printf("%d", until[(N-1)%3]);
 	return 0;
 }
