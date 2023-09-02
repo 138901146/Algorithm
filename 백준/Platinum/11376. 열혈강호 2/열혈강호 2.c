8a30b769c1494a39b1c4b5864931bc3edd494dfe
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-09-02 17:15:10 +0900

    [Platinum IV] Title: 열혈강호 2, Time: 1004 ms, Memory: 4944 KB -BaekjoonHub

---
+++ 백준/Platinum/11376. 열혈강호 2/열혈강호 2.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<stdbool.h>
+
+int *work=NULL, **employee=NULL, *employee_count=NULL;
+bool *visited=NULL;
+
+bool dfs(int current)
+{
+	for(int i=0;i<employee_count[current];++i)
+		if(!visited[employee[current][i]])
+		{
+			visited[employee[current][i]]=true;
+
+			if(work[employee[current][i]]==0 || dfs(work[employee[current][i]]))
+			{
+				work[employee[current][i]]=current;
+				return true;
+			}
+		}
+
+	return false;
+}
+
+int main(void)
+{
+	int N, M, count=0;
+
+	scanf("%d%d", &N, &M);
+	employee=(int **)malloc((N+1)*sizeof(int *));
+	employee_count=(int *)malloc((N+1)*sizeof(int));
+	work=(int *)calloc(M+1,sizeof(int));
+	visited=(bool *)malloc((M+1)*sizeof(bool));
+
+	for(int n=1;n<=N;++n)
+	{
+		scanf("%d", &employee_count[n]);
+		employee[n]=(int *)malloc(employee_count[n]*sizeof(int));
+
+		for(int i=0;i<employee_count[n];++i)
+			scanf("%d", &employee[n][i]);
+	}
+
+	for(int n=1;n<=N;++n)
+		for(int i=0;i<2;++i)
+		{
+			for(int m=1;m<=M;++m)
+				visited[m]=false;
+			dfs(n);
+		}
+
+	while(M)
+		count+=work[M--]>0;
+
+	printf("%d", count);
+	while(N)
+		free(employee[N--]);
+	free(employee);
+	free(employee_count);
+	free(work);
+	free(visited);
+	return 0;
+}
