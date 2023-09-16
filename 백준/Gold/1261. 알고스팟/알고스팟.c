c87e6345fb8f950275b5185d1996ed88b8d91a52
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-09-17 06:04:05 +0900

    [Gold IV] Title: 알고스팟, Time: 0 ms, Memory: 1304 KB -BaekjoonHub

---
+++ 백준/Gold/1261. 알고스팟/알고스팟.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int main(void)
+{
+	int N, M, min[100][100], queue[65536], front=0, rear=1, move[4][2]={{-1,0},{0,-1},{0,1},{1,0}};
+	bool wall[100][100];
+
+	scanf("%d%d", &M, &N);
+
+	for(int n=0;n<N;++n)
+		for(int m=0;m<M;++m)
+		{
+			scanf("%1d", &wall[n][m]);
+			min[n][m]=100000000;
+		}
+
+	queue[0]=min[0][0]=0;
+
+	while(front<rear)
+	{
+		int r=queue[front&65535]>>7, c=queue[front&65535]&127;
+
+		for(int i=0;i<4;++i)
+			if(r+move[i][0]>=0 && r+move[i][0]<N && c+move[i][1]>=0 && c+move[i][1]<M && min[r+move[i][0]][c+move[i][1]]>min[r][c]+wall[r+move[i][0]][c+move[i][1]])
+			{
+				min[r+move[i][0]][c+move[i][1]]=min[r][c]+wall[r+move[i][0]][c+move[i][1]];
+				queue[rear++&65535]=(r+move[i][0])<<7|c+move[i][1];
+			}
+
+		++front;
+	}
+
+	printf("%d", min[N-1][M-1]);
+	return 0;
+}
