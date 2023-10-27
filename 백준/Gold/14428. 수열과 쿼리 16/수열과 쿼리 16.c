11c9cd061ccc9f915898c796934ebe7c6fd1cd1c
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-10-27 22:32:17 +0900

    [Gold I] Title: 수열과 쿼리 16, Time: 96 ms, Memory: 2536 KB -BaekjoonHub

--- 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.c
+++ 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.c
@@ -1,80 +1,79 @@
 #include<stdio.h>
 #include<malloc.h>
 
-void init(int *A,long long *tree,int node,int start,int end)
+int *A=NULL, *tree=NULL;
+
+void init(int node,int start,int end)
 {
 	if(start==end)
-		tree[node]=(long long)start<<30|A[start];
+		tree[node]=start;
 	else
 	{
-		init(A,tree,node<<1,start,(start+end)>>1);
-		init(A,tree,node<<1|1,((start+end)>>1)+1,end);
-		tree[node]=(tree[node<<1]&1073741823)<(tree[node<<1|1]&1073741823)||(tree[node<<1]&1073741823)==(tree[node<<1|1]&1073741823)&&(tree[node<<1]>>30)<(tree[node<<1|1]>>30)?tree[node<<1]:tree[node<<1|1];
+		init(node<<1,start,(start+end)>>1);
+		init(node<<1|1,((start+end)>>1)+1,end);
+		tree[node]=A[tree[node<<1]]>A[tree[node<<1|1]]?tree[node<<1|1]:tree[node<<1];
 	}
 }
 
-long long get(long long *tree,int node,int start,int end,int left,int right)
+int get(int node,int start,int end,int left,int right)
 {
 	if(left>end || right<start)
-		return 1073741823;
+		return -1;
 	else if(left<=start && end<=right)
 		return tree[node];
 	else
 	{
-		long long left_child=get(tree,node<<1,start,(start+end)>>1,left,right), right_child=get(tree,node<<1|1,((start+end)>>1)+1,end,left,right);
+		int left_child=get(node<<1,start,(start+end)>>1,left,right), right_child=get(node<<1|1,((start+end)>>1)+1,end,left,right);
 
-		if(left_child==1073741823)
+		if(left_child==-1)
 			return right_child;
-		else if(right_child==1073741823)
+		else if(right_child==-1)
 			return left_child;
 		else
-			return (left_child&1073741823)<(right_child&1073741823)||(left_child&1073741823)==(right_child&1073741823)&&(left_child>>30)<(right_child>>30)?left_child:right_child;
+			return A[left_child]>A[right_child]?right_child:left_child;
 	}
 }
 
-void update(long long *tree,int node,int start,int end,int index,int value)
+void update(int node,int start,int end,int index,int value)
 {
-	if(index<start || index>end)
+	if(index<start || index>end || start==end)
 		return;
-	else if(start==end)
-	{
-		tree[node]=(long long)index<<30|value;
-		return;
-	}
 	else
 	{
-		update(tree,node<<1,start,(start+end)>>1,index,value);
-		update(tree,node<<1|1,((start+end)>>1)+1,end,index,value);
-		tree[node]=(tree[node<<1]&1073741823)<(tree[node<<1|1]&1073741823)||(tree[node<<1]&1073741823)==(tree[node<<1|1]&1073741823)&&(tree[node<<1]>>30)<(tree[node<<1|1]>>30)?tree[node<<1]:tree[node<<1|1];
+		update(node<<1,start,(start+end)>>1,index,value);
+		update(node<<1|1,((start+end)>>1)+1,end,index,value);
+		tree[node]=A[tree[node<<1]]>A[tree[node<<1|1]]?tree[node<<1|1]:tree[node<<1];
 	}
 }
 
 int main(void)
 {
-	int N, *A=NULL, M, size=0, operation[3];
-	long long *tree=NULL;
+	int N, M, size=0, operation[3];
 
 	scanf("%d", &N);
 	while((1<<size)<N)
-		size++;
-	size++;
+		++size;
+	++size;
 	A=(int *)malloc(N*sizeof(int));
-	tree=(long long *)malloc((1<<size)*sizeof(long long));
+	tree=(int *)malloc((1<<size)*sizeof(int));
 
-	for(int i=0;i<N;i++)
+	for(int i=0;i<N;++i)
 		scanf("%d", &A[i]);
-	init(A,tree,1,0,N-1);
+	init(1,0,N-1);
 
 	scanf("%d", &M);
-	for(int m=0;m<M;m++)
+	for(int m=0;m<M;++m)
 	{
 		for(int i=0;i<3;i++)
 			scanf("%d", &operation[i]);
 
 		if(operation[0]==1)
-			update(tree,1,0,N-1,operation[1]-1,operation[2]);
+		{
+			A[--operation[1]]=operation[2];
+			update(1,0,N-1,operation[1],operation[2]);
+		}
 		else
-			printf("%lld\n", (get(tree,1,0,N-1,operation[1]-1,operation[2]-1)>>30)+1);
+			printf("%d\n", get(1,0,N-1,operation[1]-1,operation[2]-1)+1);
 	}
 
 	free(tree);
