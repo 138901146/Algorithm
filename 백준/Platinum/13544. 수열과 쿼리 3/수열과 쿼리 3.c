8520207a735b1a83e4e6f372273eaa7ad1894175
WuHyun Park <126007127+138901146@users.noreply.github.com>
2023-12-27 08:43:31 +0900

    [Platinum III] Title: 수열과 쿼리 3, Time: 280 ms, Memory: 17072 KB -BaekjoonHub

---
+++ 백준/Platinum/13544. 수열과 쿼리 3/수열과 쿼리 3.c
@@ -0,0 +1,105 @@
+#include<stdio.h>
+#include<malloc.h>
+
+typedef struct
+{
+	int size, *list;
+}
+merge_sort_tree;
+
+void init(int *arr,merge_sort_tree *tree,int node,int start,int end)
+{
+	if(start==end)
+	{
+		tree[node].size=1;
+		tree[node].list=(int *)malloc(sizeof(int));
+		tree[node].list[0]=arr[start];
+	}
+	else
+	{
+		int left=node<<1, right=left|1, left_size=0, right_size=0;
+
+		init(arr,tree,left,start,(start+end)>>1);
+		init(arr,tree,right,((start+end)>>1)+1,end);
+		tree[node].size=tree[left].size+tree[right].size;
+		tree[node].list=(int *)malloc(tree[node].size*sizeof(int));
+
+		for(int i=0;i<tree[node].size;++i)
+			if(left_size==tree[left].size)
+				tree[node].list[i]=tree[right].list[right_size++];
+			else if(right_size==tree[right].size)
+				tree[node].list[i]=tree[left].list[left_size++];
+			else if(tree[left].list[left_size]<tree[right].list[right_size])
+				tree[node].list[i]=tree[left].list[left_size++];
+			else
+				tree[node].list[i]=tree[right].list[right_size++];
+	}
+}
+
+int get(merge_sort_tree *tree,int node,int start,int end,int left,int right,int k)
+{
+	if(left>end || right<start)
+		return 0;
+	else if(left<=start && end<=right)
+	{
+		int low=0, high=end-start+1, mid, ans=end-start+1;
+
+		while(low<=high)
+		{
+			mid=low+high>>1;
+
+			if(tree[node].list[mid]<=k)
+				low=mid+1;
+			else
+			{
+				ans=mid;
+				high=mid-1;
+			}
+		}
+
+		return end-start+1-ans;
+	}
+	else
+		return get(tree,node<<1,start,(start+end)>>1,left,right,k)+get(tree,node<<1|1,((start+end)>>1)+1,end,left,right,k);
+}
+
+void free_tree(merge_sort_tree *tree,int node,int start,int end)
+{
+	if(start!=end)
+	{
+		free_tree(tree,node<<1,start,(start+end)>>1);
+		free_tree(tree,node<<1|1,(start+end>>1)+1,end);
+	}
+	free(tree[node].list);
+}
+
+int main(void)
+{
+	int N, M, k, *A=NULL, a, b, c, size=0, last_ans=0;
+	merge_sort_tree *tree=NULL;
+
+	scanf("%d", &N);
+	while((1<<size)<=N)
+		++size;
+	A=(int *)malloc((N+1)*sizeof(int));
+	tree=(merge_sort_tree *)malloc((1<<++size)*sizeof(merge_sort_tree));
+
+	for(int i=1;i<=N;++i)
+		scanf("%d", &A[i]);
+
+	init(A,tree,1,1,N);
+
+	scanf("%d", &M);
+	while(M--)
+	{
+		scanf("%d%d%d", &a, &b, &c);
+
+		last_ans=get(tree,1,1,N,a^last_ans,b^last_ans,c^last_ans);
+		printf("%d\n", last_ans);
+	}
+
+	free(A);
+	free_tree(tree,1,1,N);
+	free(tree);
+	return 0;
+}
