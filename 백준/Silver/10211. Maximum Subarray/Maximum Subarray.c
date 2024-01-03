2f6f2af5b50912ef5e581777b21e75a8f60b7ab2
WuHyun Park <0x847769a@gmail.com>
2024-01-03 22:19:29 +0900

    [Silver IV] Title: Maximum Subarray, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/10211. Maximum Subarray/Maximum Subarray.c
+++ 백준/Silver/10211. Maximum Subarray/Maximum Subarray.c
@@ -1,18 +1,4 @@
 #include<stdio.h>
-#include<stdlib.h>
-
-int maximum_subarray(int *arr,int size)
-{
-	int sum=0, max=arr[0];
-
-	for(int i=0;i<size;i++)
-	{
-		sum=arr[i]+sum>arr[i]?arr[i]+sum:arr[i];
-		max=max>sum?max:sum;
-	}
-
-	return max;
-}
 
 int main(void)
 {
@@ -20,18 +6,21 @@ int main(void)
 
 	scanf("%d", &T);
 
-	for(int t=0;t<T;t++)
+	while(T--)
 	{
-		int N, *X=NULL;
+		int N, X, sum=0, max=-1000000;
 
 		scanf("%d", &N);
-		X=(int *)malloc(N*sizeof(int));
 
-		for(int n=0;n<N;n++)
-			scanf("%d", &X[n]);
+		while(N--)
+		{
+			scanf("%d", &X);
 
-		printf("%d\n", maximum_subarray(X,N));
-		free(X);
+			sum=X+sum>X?X+sum:X;
+			max=max>sum?max:sum;
+		}
+
+		printf("%d\n", max);
 	}
 
 	return 0;
