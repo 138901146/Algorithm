9cd4db11d1af477e56174525a8590de43b9104d8
WuHyun Park <0x847769a@gmail.com>
2024-01-12 01:00:44 +0900

    [Silver I] Title: 회의실 배정, Time: 40 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.c
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.c
@@ -1,78 +1,37 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int stemp[100000], etemp[100000];
-
-void merge(int *start,int *end,int left,int right,int mid)
+typedef struct
 {
-	int i=left, j=mid+1;
-
-	for(int k=left;k<=right;k++)
-		if(j>right)
-		{
-			stemp[k]=start[i];
-			etemp[k]=end[i];
-			i++;
-		}
-		else if(i>mid)
-		{
-			stemp[k]=start[j];
-			etemp[k]=end[j];
-			j++;
-		}
-		else if(end[i]<end[j] || end[i]==end[j] && start[i]<start[j])
-		{
-			stemp[k]=start[i];
-			etemp[k]=end[i];
-			i++;
-		}
-		else
-		{
-			stemp[k]=start[j];
-			etemp[k]=end[j];
-			j++;
-		}
-
-	for(int k=left;k<=right;k++)
-	{
-		start[k]=stemp[k];
-		end[k]=etemp[k];
-	}
+	int start, end;
 }
+term;
 
-void mergesort(int *start,int *end,int left,int right)
+int compare(const void *x,const void *y)
 {
-	if(left<right)
-	{
-		int mid=(left+right)/2;
-		mergesort(start,end,left,mid);
-		mergesort(start,end,mid+1,right);
-		merge(start,end,left,right,mid);
-	}
+	return ((term *)x)->end<((term *)y)->end?-1:((term *)x)->end>((term *)y)->end?1:((term *)x)->start<((term *)y)->start?-1:1;
 }
 
 int main(void)
 {
-	int N, *start=NULL, *end=NULL, count=0, last=-1;
+	int N, count=0, last=-1;
+	term *meeting=NULL;
 
 	scanf("%d", &N);
-	start=(int *)malloc(N*sizeof(int));
-	end=(int *)malloc(N*sizeof(int));
+	meeting=(term *)malloc(N*sizeof(term));
 
-	for(int n=0;n<N;n++)
-		scanf("%d%d", &start[n], &end[n]);
+	for(int n=0;n<N;++n)
+		scanf("%d%d", &meeting[n].start, &meeting[n].end);
+	qsort((void *)meeting,(size_t)N,sizeof(term),compare);
 
-	mergesort(start,end,0,N-1);
-
-	for(int n=0;n<N;n++)
-		if(start[n]>=last)
+	for(int n=0;n<N;++n)
+		if(meeting[n].start>=last)
 		{
-			count++;
-			last=end[n];
+			++count;
+			last=meeting[n].end;
 		}
 
-	printf("%d\n", count);
-	free(start);
-	free(end);
+	printf("%d", count);
+	free(meeting);
 	return 0;
 }
