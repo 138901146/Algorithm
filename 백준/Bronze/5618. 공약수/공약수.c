5ea371cfd8b26674de2bd963b85d73a56e876df5
WuHyun Park <0x847769a@gmail.com>
2024-01-16 23:04:43 +0900

    [Bronze II] Title: 공약수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/5618. 공약수/공약수.c
+++ 백준/Bronze/5618. 공약수/공약수.c
@@ -2,16 +2,18 @@
 
 int gcd(int x,int y)
 {
+	int temp;
+
 	if(x<y)
 	{
-		int temp=x;
+		temp=x;
 		x=y;
 		y=temp;
 	}
 
-	while(y!=0)
+	while(y)
 	{
-		int temp=x%y;
+		temp=x%y;
 		x=y;
 		y=temp;
 	}
@@ -21,17 +23,45 @@ int gcd(int x,int y)
 
 int main(void)
 {
-	int n, num[3], gcd_num;
+	int n, number, gcd_value, divisor[100]={1}, divisor_count=1, root;
 
 	scanf("%d", &n);
-	for(int i=0;i<n;i++)
-		scanf("%d", &num[i]);
 
-	gcd_num=n==2?gcd(num[0],num[1]):gcd(gcd(num[0],num[1]),num[2]);
+	scanf("%d%d", &gcd_value, &number);
 
-	for(int i=1;i<=gcd_num;i++)
-		if(gcd_num%i==0)
-			printf("%d\n", i);
+	gcd_value=gcd(gcd_value,number);
 
+	if(n==3)
+	{
+		scanf("%d", &number);
+		gcd_value=gcd(gcd_value,number);
+	}
+
+	if(gcd_value>1)
+	{
+		divisor[divisor_count++]=gcd_value;
+
+		for(root=2;root*root<gcd_value;++root)
+			if(gcd_value%root==0)
+			{
+				divisor[divisor_count++]=root;
+				divisor[divisor_count++]=gcd_value/root;
+			}
+
+		if(root*root==gcd_value)
+			divisor[divisor_count++]=root;
+	}
+
+	for(int i=1;i<divisor_count;++i)
+		for(int j=i+1;j<divisor_count;++j)
+			if(divisor[i]>divisor[j])
+			{
+				int temp=divisor[i];
+				divisor[i]=divisor[j];
+				divisor[j]=temp;
+			}
+
+	for(int i=0;i<divisor_count;++i)
+		printf("%d\n", divisor[i]);
 	return 0;
 }
