6fa18f8d057978aa5c0a5fe187b23927d15fe6e1
WuHyun Park <0x847769a@gmail.com>
2024-01-24 00:49:56 +0900

    [Silver II] Title: 뒤집어진 소수, Time: 760 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/10859. 뒤집어진 소수/뒤집어진 소수.c
+++ 백준/Silver/10859. 뒤집어진 소수/뒤집어진 소수.c
@@ -1,62 +1,41 @@
 #include<stdio.h>
-#include<stdbool.h>
 
-bool is_prime(long long x)
+int is_prime(long long x)
 {
 	if(x<2)
-		return false;
-	else if(x==2)
-		return true;
-	else if(x%2==0)
-		return false;
+		return 0;
+	if(x==2)
+		return 1;
+	if(!(x&1))
+		return 0;
 	for(long long i=3;i*i<=x;i+=2)
-		if(x%i==0)
-			return false;
-	return true;
+		if(!(x%i))
+			return 0;
+	return 1;
 }
 
 int main(void)
 {
-	long long original, reverse=0;
-	bool able=true;
+	int map[10]={0,1,2,-1,-1,5,9,-1,8,6};
+	long long N, temp, reversed=0;
 
-	scanf("%lld", &original);
+	scanf("%lld", &N);
 
-	if(!is_prime(original))
-		able=false;
-	else
-		while(original>0)
+	temp=N;
+	while(temp)
+	{
+		if(map[temp%10]==-1)
 		{
-			switch(original%10)
-			{
-				case 0:
-				case 2:
-				case 5:
-				case 8:
-				case 1:
-					break;
-				case 6:
-					original+=3;
-					break;
-				case 9:
-					original-=3;
-					break;
-				default:
-					able=false;
-					break;
-			}
-
-			if(!able)
-				break;
-
-			reverse*=10;
-			reverse+=original%10;
-			original/=10;
+			printf("no");
+			return 0;
 		}
 
-	if(able&&is_prime(reverse))
-		printf("yes\n");
-	else
-		printf("no\n");
+		reversed*=10;
+		reversed+=map[temp%10];
+
+		temp/=10;
+	}
+
+	printf("%s", is_prime(N)&&is_prime(reversed)?"yes":"no");
 	return 0;
 }
