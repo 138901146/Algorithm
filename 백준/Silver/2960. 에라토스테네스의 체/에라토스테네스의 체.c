20feaf17f908827e69611cb0f8c765d4d3cde8fc
WuHyun Park <0x847769a@gmail.com>
2024-01-24 00:41:00 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.c
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.c
@@ -1,37 +1,31 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<stdbool.h>
 
 int main(void)
 {
-	int N, K, count=0;
-	bool *sieve=NULL, reached=false;
+	int N, K;
+	bool prime[1001];
 
-	scanf("%d %d", &N, &K);
-	sieve=(bool *)calloc(N+1,sizeof(bool));
+	for(int i=1000;i;--i)
+		prime[i]=true;
 
-	for(int i=2;;i++)
-	{
-		if(!sieve[i])
-			for(int j=1;i*j<=N;j++)
-			{
-				if(!sieve[i*j])
+	scanf("%d%d", &N, &K);
+
+	for(int i=2;i<=N;++i)
+		if(prime[i])
+			for(int j=i;j<=N;j+=i)
+				if(prime[j])
 				{
-					sieve[i*j]=true;
-					count++;
-				}
+					--K;
 
-				if(count==K)
-				{
-					reached=true;
-					printf("%d\n", i*j);
-					free(sieve);
-					break;
+					if(!K)
+					{
+						printf("%d", j);
+						return 0;
+					}
+
+					prime[j]=false;
 				}
-			}
-		if(reached)
-			break;
-	}
 
 	return 0;
 }
