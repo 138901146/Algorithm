2df4b33f486b45303336096fb2c989d6cf9e8f48
WuHyun Park <0x847769a@gmail.com>
2024-01-28 06:00:22 +0900

    [Gold III] Title: 두 배열의 합, Time: 132 ms, Memory: 20660 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.c
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.c
@@ -3,7 +3,7 @@
 
 int compare(const void *x,const void *y)
 {
-	return *(long long *)x>*(long long *)y?1:*(long long *)x==*(long long *)y?0:-1;
+	return *(long long *)x>*(long long *)y?1:-1;
 }
 
 int main(void)
@@ -17,11 +17,11 @@ int main(void)
 	A=(long long *)malloc((N+1)*sizeof(long long));
 	sum_A=(long long *)malloc(N*N*sizeof(long long));
 	A[0]=0;
-	for(int n=1;n<=N;n++)
+	for(int n=1;n<=N;++n)
 	{
 		scanf("%lld", &A[n]);
 		A[n]+=A[n-1];
-		for(int i=0;i<n;i++)
+		for(int i=0;i<n;++i)
 			sum_A[a++]=A[n]-A[i];
 	}
 	free(A);
@@ -31,18 +31,18 @@ int main(void)
 	B=(long long *)malloc((M+1)*sizeof(long long));
 	sum_B=(long long *)malloc(M*M*sizeof(long long));
 	B[0]=0;
-	for(int m=1;m<=M;m++)
+	for(int m=1;m<=M;++m)
 	{
 		scanf("%lld", &B[m]);
 		B[m]+=B[m-1];
-		for(int i=0;i<m;i++)
+		for(int i=0;i<m;++i)
 			sum_B[b++]=B[m]-B[i];
 	}
 	free(B);
 	qsort((void *)sum_B,(size_t)b,sizeof(long long),compare);
 
 	sum=(long long *)calloc(a+1,sizeof(long long));
-	for(int i=0;i<a;i++)
+	for(int i=0;i<a;++i)
 	{
 		sum[i+1]=sum[i];
 		if(i>0 && sum_A[i]==sum_A[i-1])
@@ -51,11 +51,11 @@ int main(void)
 			continue;
 		}
 
-		int left=0, right=b-1, mid=(left+right)>>1;
+		int left=0, right=b-1, mid;
 
 		while(left+1<right)
 		{
-			mid=(left+right)>>1;
+			mid=left+right>>1;
 
 			if(sum_A[i]+sum_B[mid]==T)
 				break;
@@ -65,44 +65,44 @@ int main(void)
 				right=mid;
 		}
 
-		mid=(left+right)>>1;
+		mid=left+right>>1;
 		if(sum_A[i]+sum_B[mid]==T)
 		{
-			sum[i+1]++;
+			++sum[i+1];
 			left=mid-1;
 			right=mid+1;
 
 			while(left>=0 && sum_A[i]+sum_B[left]==T)
 			{
-				sum[i+1]++;
-				left--;
+				++sum[i+1];
+				--left;
 			}
 			while(right<b && sum_A[i]+sum_B[right]==T)
 			{
-				sum[i+1]++;
-				right++;
+				++sum[i+1];
+				++right;
 			}
 		}
 		else if(sum_A[i]+sum_B[mid+1]==T)
 		{
-			sum[i+1]++;
+			++sum[i+1];
 			left=mid;
 			right=mid+2;
 
 			while(left>=0 && sum_A[i]+sum_B[left]==T)
 			{
-				sum[i+1]++;
-				left--;
+				++sum[i+1];
+				--left;
 			}
 			while(right<b && sum_A[i]+sum_B[right]==T)
 			{
-				sum[i+1]++;
-				right++;
+				++sum[i+1];
+				++right;
 			}
 		}
 	}
 
-	printf("%lld\n", sum[a]);
+	printf("%lld", sum[a]);
 	free(sum);
 	free(sum_A);
 	free(sum_B);
