f45e3b96e6ac941ee1a833f9b9b9d9d8db18d800
WuHyun Park <0x847769a@gmail.com>
2024-01-28 06:48:15 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 2092 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.c
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.c
@@ -1,26 +1,29 @@
 #include<stdio.h>
+#include<stdbool.h>
 
-int isPrime(int N)
-{
-	if(N==2)
-		return 1;
-	else if(N==1 || N%2==0)
-		return 0;
-	for(int i=3;i*i<=N;i+=2)
-		if(N%i==0)
-			return 0;
-	return 1;
-}
+bool prime[1000002]={false};
 
 int main(void)
 {
 	int M, N;
 
-	scanf("%d %d", &M, &N);
+	scanf("%d%d", &M, &N);
 
-	for(int i=M;i<=N;i++)
-		if(isPrime(i))
-			printf("%d\n", i);
+	for(int n=N|1;n>2;n-=2)
+		prime[n]=true;
+
+	if(M<3)
+		printf("2\n");
+	for(int i=3;i<=N;i+=2)
+		if(prime[i])
+		{
+			if(M<=i)
+				printf("%d\n", i);
+
+			if(i*i<=N)
+				for(int j=i<<1;j<=N;j+=i)
+					prime[j]=false;
+		}
 
 	return 0;
 }
