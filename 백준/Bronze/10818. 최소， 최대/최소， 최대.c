ba5eaadc0240763c20e37a030147b529f37ab357
WuHyun Park <0x847769a@gmail.com>
2024-01-28 06:59:27 +0900

    [Bronze III] Title: 최소, 최대, Time: 172 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.c
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.c
@@ -2,19 +2,17 @@
 
 int main(void)
 {
-	int N, min=1000000, max=-1000000;
+	int N, value, min=1000000, max=-1000000;
 
 	scanf("%d", &N);
 
-	for(int i=0;i<N;i++)
+	while(N--)
 	{
-		int value;
-
 		scanf("%d", &value);
 		min=value<min?value:min;
 		max=value>max?value:max;
 	}
-	printf("%d %d\n", min, max);
 
+	printf("%d %d", min, max);
 	return 0;
 }
