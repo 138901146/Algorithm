56174d6578fab027e90785e2c6f24bba3727b412
WuHyun Park <0x847769a@gmail.com>
2024-02-02 21:55:49 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.c
+++ 백준/Bronze/1037. 약수/약수.c
@@ -1,30 +1,9 @@
 #include<stdio.h>
 
-long long lcm(long long x,long long y)
-{
-	long long mul=x*y, temp;
-
-	if(x<y)
-	{
-		temp=x;
-		x=y;
-		y=temp;
-	}
-
-	while(y)
-	{
-		temp=x%y;
-		x=y;
-		y=temp;
-	}
-
-	return mul/x;
-}
-
 int main(void)
 {
 	int count;
-	long long N=1, A, max=0, min=10000000;
+	long long A, max=0, min=10000000;
 
 	scanf("%d", &count);
 
@@ -34,10 +13,8 @@ int main(void)
 
 		max=A>max?A:max;
 		min=A<min?A:min;
-
-		N=lcm(N,A);
 	}
 
-	printf("%lld", N==max?N*min:N);
+	printf("%lld", max*min);
 	return 0;
 }
