36276098fd87b1cac040ddb02806e0eb8111d899
WuHyun Park <0x847769a@gmail.com>
2024-02-05 22:27:50 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 200 ms, Memory: 5208 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.c
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.c
@@ -9,23 +9,23 @@ int main(void)
 	sum=(int **)malloc((N+1)*sizeof(int *));
 
 	sum[0]=(int *)calloc(N+1,sizeof(int));
-	for(int i=1;i<=N;i++)
+	for(int i=1;i<=N;++i)
 	{
 		sum[i]=(int *)calloc(N+1,sizeof(int));
-		for(int j=1;j<=N;j++)
+		for(int j=1;j<=N;++j)
 		{
 			scanf("%d", &sum[i][j]);
 			sum[i][j]=sum[i][j]+sum[i-1][j]+sum[i][j-1]-sum[i-1][j-1];
 		}
 	}
 
-	for(int m=0;m<M;m++)
+	while(M--)
 	{
 		scanf("%d%d%d%d", &x1, &y1, &x2, &y2);
 		printf("%d\n", sum[x2][y2]-sum[x2][y1-1]-sum[x1-1][y2]+sum[x1-1][y1-1]);
 	}
 
-	for(int n=0;n<=N;n++)
+	for(int n=N;n>=0;--n)
 		free(sum[n]);
 	free(sum);
 	return 0;
