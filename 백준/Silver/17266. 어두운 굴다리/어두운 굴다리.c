da8eb2fbb890d948945e51f444019556b1035aa7
WuHyun Park <0x847769a@gmail.com>
2024-02-08 23:32:29 +0900

    [Silver IV] Title: 어두운 굴다리, Time: 12 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/17266. 어두운 굴다리/어두운 굴다리.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int N, M, x[2], current=1, previous=0, gap;
+
+	scanf("%d", &N);
+	scanf("%d", &M);
+
+	scanf("%d", &x[0]);
+	gap=x[0];
+	while(--M)
+	{
+		scanf("%d", &x[current]);
+		gap=gap<(x[current]-x[previous]+1>>1)?x[current]-x[previous]+1>>1:gap;
+		current^=1;
+		previous^=1;
+	}
+	gap=gap<N-x[previous]?N-x[previous]:gap;
+
+	printf("%d", gap);
+	return 0;
+}
