5af19e140ab9b71bda92670f9cab67a3242e2a46
WuHyun Park <0x847769a@gmail.com>
2024-02-17 04:42:13 +0900

    [Silver I] Title: 회전 초밥, Time: 4 ms, Memory: 1124 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.c
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.c
@@ -1,42 +1,49 @@
 #include<stdio.h>
-#include<stdlib.h>
 
 int main(void)
 {
-	int N, d, k, c, *plate=NULL, *ate=NULL, max=0, count=0;
+	int N, d, k, c, belt[30000], sushi[3001]={0}, max, count=0;
 
 	scanf("%d%d%d%d", &N, &d, &k, &c);
-	plate=(int *)malloc(N*sizeof(int));
-	ate=(int *)calloc(d+1,sizeof(int));
 
-	for(int n=0;n<N;n++)
-		scanf("%d", &plate[n]);
-
-	for(int i=0;i<k;i++)
-		ate[plate[i]]++;
-	ate[c]++;
-
-	for(int i=1;i<=d;i++)
-		if(ate[i]>0)
-			count++;
-	max=count;
-
-	for(int i=1;i<N;i++)
+	for(int i=0;i<k;++i)
 	{
-		if(ate[plate[i-1]]==1)
-			count--;
-		ate[plate[i-1]]--;
-		if(ate[plate[(i+k-1)%N]]==0)
-		{
-			count++;
-			if(count>max)
-				max=count;
-		}
-		ate[plate[(i+k-1)%N]]++;
+		scanf("%d", &belt[i]);
+
+		if(!sushi[belt[i]])
+			++count;
+		++sushi[belt[i]];
+	}
+	max=count+(!sushi[c]);
+
+	for(int i=k;i<N;++i)
+	{
+		scanf("%d", &belt[i]);
+
+		--sushi[belt[i-k]];
+		if(!sushi[belt[i-k]])
+			--count;
+
+		if(!sushi[belt[i]])
+			++count;
+		++sushi[belt[i]];
+
+		max=max<count+!sushi[c]?count+!sushi[c]:max;
 	}
 
-	printf("%d\n", max);
-	free(ate);
-	free(plate);
+	for(int i=0;i<k;++i)
+	{
+		--sushi[belt[N+i-k]];
+		if(!sushi[belt[N+i-k]])
+			--count;
+
+		if(!sushi[belt[i]])
+			++count;
+		++sushi[belt[i]];
+
+		max=max<count+!sushi[c]?count+!sushi[c]:max;
+	}
+
+	printf("%d", max);
 	return 0;
 }
