68c6ce56042c3cb898ac0506fffc03b361f521b3
WuHyun Park <0x847769a@gmail.com>
2024-02-20 22:14:43 +0900

    [Silver III] Title: 수열, Time: 16 ms, Memory: 1308 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.c
+++ 백준/Silver/2559. 수열/수열.c
@@ -1,25 +1,32 @@
 #include<stdio.h>
+#include<malloc.h>
 
 int main(void)
 {
-	int N, K, *temperature=NULL, sum=0, max=-1000000000;
+	int N, K, *temperature=NULL, sum=0, max;
 
 	scanf("%d%d", &N, &K);
-	temperature=(int *)malloc(N*sizeof(int));
+	temperature=(int *)malloc(K*sizeof(int));
 
-	for(int n=0;n<N;n++)
-		scanf("%d", &temperature[n]);
-
-	for(int k=0;k<K;k++)
-		sum+=temperature[k];
-	max=sum;
-	for(int k=K;k<N;k++)
+	for(int k=0;k<K;++k)
 	{
-		sum=sum-temperature[k-K]+temperature[k];
-		max=sum>max?sum:max;
+		scanf("%d", &temperature[k]);
+		sum+=temperature[k];
 	}
 
-	printf("%d\n", max);
+	max=sum;
+	for(int i=K;i<N;++i)
+	{
+		int index=i%K;
+
+		sum-=temperature[index];
+		scanf("%d", &temperature[index]);
+		sum+=temperature[index];
+
+		max=max<sum?sum:max;
+	}
+
+	printf("%d", max);
 	free(temperature);
 	return 0;
 }
