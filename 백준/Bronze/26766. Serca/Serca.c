20c8fa487edf619af01a3c46922ee0cf21c80247
WuHyun Park <0x847769a@gmail.com>
2024-03-19 14:02:49 +0900

    [Bronze V] Title: Serca, Time: 136 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int N;
+
+	scanf("%d", &N);
+
+	while(N--)
+	{
+		printf(" @@@   @@@ \n");
+		printf("@   @ @   @\n");
+		printf("@    @    @\n");
+		printf("@         @\n");
+		printf(" @       @ \n");
+		printf("  @     @  \n");
+		printf("   @   @   \n");
+		printf("    @ @    \n");
+		printf("     @     \n");
+	}
+
+	return 0;
+}
