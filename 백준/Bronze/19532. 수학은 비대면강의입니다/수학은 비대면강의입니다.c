bbb358155aff37ee43582deb66096412a35a480b
WuHyun Park <0x847769a@gmail.com>
2024-04-28 10:09:55 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.c
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.c
@@ -4,9 +4,8 @@ int main(void)
 {
 	int a, b, c, d, e, f;
 
-	scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+	scanf("%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f);
 
-	printf("%d %d\n", (b*f-c*e)/(b*d-a*e), (a*f-c*d)/(a*e-b*d));
-
+	printf("%d %d", (b*f-c*e)/(b*d-a*e), (a*f-c*d)/(a*e-b*d));
 	return 0;
 }
