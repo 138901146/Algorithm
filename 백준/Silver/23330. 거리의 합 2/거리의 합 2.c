d1bc62e7fa5ebe3d48080316a0042353257b401b
WuHyun Park <0x847769a@gmail.com>
2024-04-29 22:57:23 +0900

    [Silver III] Title: 거리의 합 2, Time: 156 ms, Memory: 5024 KB -BaekjoonHub

--- 백준/Silver/23330. 거리의 합 2/거리의 합 2.c
+++ 백준/Silver/23330. 거리의 합 2/거리의 합 2.c
@@ -3,25 +3,25 @@
 
 int compare(const void *x,const void *y)
 {
-	return *(long long *)x<*(long long *)y?-1:1;
+	return *(int *)x<*(int *)y?-1:1;
 }
 
 int main(void)
 {
-	int n;
-	long long *x=NULL, sum=0, distance=0;
+	int n, *x=NULL;
+	long long sum=0, distance=0;
 
 	scanf("%d", &n);
-	x=(long long *)malloc(n*sizeof(long long));
+	x=(int *)malloc(n*sizeof(int));
 
 	for(int i=0;i<n;++i)
-		scanf("%lld", &x[i]);
-	qsort((void *)x,(size_t)n,sizeof(long long),compare);
+		scanf("%d", &x[i]);
+	qsort((void *)x,(size_t)n,sizeof(int),compare);
 
 	sum=x[0];
 	for(int i=1;i<n;++i)
 	{
-		distance+=i*x[i]-sum;
+		distance+=(long long)i*x[i]-sum;
 		sum+=x[i];
 	}
 
