402e776896f36f740d98434d9a22b5efe3b077a9
WuHyun Park <0x847769a@gmail.com>
2024-05-15 12:28:40 +0900

    [Gold IV] Title: 출근 경로, Time: 0 ms, Memory: 1152 KB -BaekjoonHub

--- 백준/Gold/5569. 출근 경로/출근 경로.c
+++ 백준/Gold/5569. 출근 경로/출근 경로.c
@@ -4,17 +4,19 @@
 
 int main(void)
 {
-	int w, h, map[101][101][4]={0};
+	int w, h, map[100][100][4]={0};
 
 	scanf("%d%d", &w, &h);
 
-	for(int i=1;i<=w;++i)
-		map[i][1][0]=1;
-	for(int j=1;j<=h;++j)
-		map[1][j][1]=1;
+	--w;
+	--h;
+	for(int i=0;i<=w;++i)
+		map[i][0][0]=1;
+	for(int j=0;j<=h;++j)
+		map[0][j][1]=1;
 
-	for(int j=2;j<=h;++j)
-		for(int i=2;i<=w;++i)
+	for(int j=1;j<=h;++j)
+		for(int i=1;i<=w;++i)
 		{
 			map[i][j][0]=(map[i-1][j][0]+map[i-1][j][2])%MOD;
 			map[i][j][1]=(map[i][j-1][1]+map[i][j-1][3])%MOD;
