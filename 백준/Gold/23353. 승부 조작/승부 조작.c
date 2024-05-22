956c2c1f2bb48a81beb42624b037bfb8a827c234
WuHyun Park <0x847769a@gmail.com>
2024-05-22 22:31:57 +0900

    [Gold III] Title: 승부 조작, Time: 100 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/23353. 승부 조작/승부 조작.c
+++ 백준/Gold/23353. 승부 조작/승부 조작.c
@@ -1,40 +1,43 @@
 #include<stdio.h>
 
-int stone[1001][1002], score[1001][1002][4][2]={0};
-
 int main(void)
 {
-	int N, max=0;
+	int N, stone, score[2][1002][4][2]={0}, current=1, previous=0, max=0;
 
 	scanf("%d", &N);
 
-	for(int i=1;i<=N;++i)
+	for(int i=0;i<N;++i, current^=1, previous^=1)
 		for(int j=1;j<=N;++j)
 		{
-			scanf("%d", &stone[i][j]);
+			scanf("%d", &stone);
 
-			if(stone[i][j]==1)
+			if(stone==1)
 			{
-				score[i][j][0][0]=score[i][j-1][0][0]+1;
-				score[i][j][1][0]=score[i-1][j][1][0]+1;
-				score[i][j][2][0]=score[i-1][j-1][2][0]+1;
-				score[i][j][3][0]=score[i-1][j+1][3][0]+1;
-				score[i][j][0][1]=(score[i][j-1][0][0]<score[i][j-1][0][1]?score[i][j-1][0][1]:score[i][j-1][0][0])+1;
-				score[i][j][1][1]=(score[i-1][j][1][0]<score[i-1][j][1][1]?score[i-1][j][1][1]:score[i-1][j][1][0])+1;
-				score[i][j][2][1]=(score[i-1][j-1][2][0]<score[i-1][j-1][2][1]?score[i-1][j-1][2][1]:score[i-1][j-1][2][0])+1;
-				score[i][j][3][1]=(score[i-1][j+1][3][0]<score[i-1][j+1][3][1]?score[i-1][j+1][3][1]:score[i-1][j+1][3][0])+1;
+				score[current][j][0][0]=score[current][j-1][0][0]+1;
+				score[current][j][1][0]=score[previous][j][1][0]+1;
+				score[current][j][2][0]=score[previous][j-1][2][0]+1;
+				score[current][j][3][0]=score[previous][j+1][3][0]+1;
+				score[current][j][0][1]=(score[current][j-1][0][0]<score[current][j-1][0][1]?score[current][j-1][0][1]:score[current][j-1][0][0])+1;
+				score[current][j][1][1]=(score[previous][j][1][0]<score[previous][j][1][1]?score[previous][j][1][1]:score[previous][j][1][0])+1;
+				score[current][j][2][1]=(score[previous][j-1][2][0]<score[previous][j-1][2][1]?score[previous][j-1][2][1]:score[previous][j-1][2][0])+1;
+				score[current][j][3][1]=(score[previous][j+1][3][0]<score[previous][j+1][3][1]?score[previous][j+1][3][1]:score[previous][j+1][3][0])+1;
 			}
-			else if(stone[i][j]==2)
+			else if(stone==2)
 			{
-				score[i][j][0][1]=score[i][j-1][0][0]+1;
-				score[i][j][1][1]=score[i-1][j][1][0]+1;
-				score[i][j][2][1]=score[i-1][j-1][2][0]+1;
-				score[i][j][3][1]=score[i-1][j+1][3][0]+1;
+				score[current][j][0][0]=score[current][j][1][0]=score[current][j][2][0]=score[current][j][3][0]=0;
+				score[current][j][0][1]=score[current][j-1][0][0]+1;
+				score[current][j][1][1]=score[previous][j][1][0]+1;
+				score[current][j][2][1]=score[previous][j-1][2][0]+1;
+				score[current][j][3][1]=score[previous][j+1][3][0]+1;
 			}
+			else
+				for(int k=0;k<4;++k)
+					for(int l=0;l<2;++l)
+						score[current][j][k][l]=0;
 
 			for(int k=0;k<4;++k)
 				for(int l=0;l<2;++l)
-					max=score[i][j][k][l]<max?max:score[i][j][k][l];
+					max=score[current][j][k][l]<max?max:score[current][j][k][l];
 		}
 
 	printf("%d", max);
