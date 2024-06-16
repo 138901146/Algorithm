17e31717d41a7a77016b012c299ed6d52c8ad561
WuHyun Park <0x847769a@gmail.com>
2024-06-17 06:08:35 +0900

    [Gold II] Title: 78계단 내려가기 대회, Time: 168 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Gold/31783. 78계단 내려가기 대회/78계단 내려가기 대회.c
+++ 백준/Gold/31783. 78계단 내려가기 대회/78계단 내려가기 대회.c
@@ -3,31 +3,30 @@
 
 int main(void)
 {
-	int N, *H=NULL, *A=NULL, *B=NULL;
+	int N, *H=NULL, *A=NULL, B;
 	long long *max=NULL;
 
 	scanf("%d", &N);
 	H=(int *)malloc(N*sizeof(int));
 	A=(int *)malloc(N*sizeof(int));
-	B=(int *)malloc(N*sizeof(int));
 	max=(long long *)calloc(N,sizeof(long long));
 
 	for(int i=0;i<N;++i)
 		scanf("%d", &H[i]);
 	for(int i=1;i<N;++i)
 		scanf("%d", &A[i]);
-	for(int i=1;i<N;++i)
-		scanf("%d", &B[i]);
 
 	for(int i=1;i<N;++i)
 	{
 		int left=0, right=i-1, mid, point=i-1;
 
+		scanf("%d", &B);
+
 		while(left<=right)
 		{
 			mid=left+right>>1;
 
-			if(H[mid]<H[i]+B[i])
+			if(H[mid]<H[i]+B)
 				right=mid-1;
 			else
 			{
@@ -36,7 +35,7 @@ int main(void)
 			}
 		}
 
-		if(H[i]+B[i]<=H[point])
+		if(H[i]+B<=H[point])
 			max[i]=max[point]+A[i];
 		max[i]=max[i]<max[i-1]?max[i-1]:max[i];
 	}
@@ -44,7 +43,6 @@ int main(void)
 	printf("%lld", max[N-1]);
 	free(H);
 	free(A);
-	free(B);
 	free(max);
 	return 0;
 }
