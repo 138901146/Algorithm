3caa427a7df6dbd72583ee30b8edbd544e217998
WuHyun Park <0x847769a@gmail.com>
2024-06-29 09:00:33 +0900

    [Silver V] Title: 문서 검색, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+	char document[2502]={0}, word[52]={0};
+	int document_length, word_length, count=0;
+
+	fgets(document,sizeof(document),stdin);
+	fgets(word,sizeof(word),stdin);
+
+	document_length=strlen(document);
+	word_length=strlen(word);
+
+	if(document[document_length-1]=='\n')
+		document[--document_length]=0;
+	if(word[word_length-1]=='\n')
+		word[--word_length]=0;
+
+	if(!word_length || !document_length)
+	{
+		printf("0");
+		return 0;
+	}
+
+	for(int i=0;i+word_length<=document_length;++i)
+	{
+		int current=0;
+
+		while(current<word_length && word[current]==document[i+current])
+			++current;
+
+		if(current==word_length)
+		{
+			++count;
+			i+=word_length-1;
+		}
+	}
+
+	printf("%d", count);
+	return 0;
+}
