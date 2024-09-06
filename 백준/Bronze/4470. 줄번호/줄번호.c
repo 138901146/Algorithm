e1185f0d0eb4c243da7732507997e2a4b2b13619
WuHyun Park <0x847769a@gmail.com>
2024-09-07 06:00:25 +0900

    [Bronze IV] Title: 줄번호, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/4470. 줄번호/줄번호.c
+++ 백준/Bronze/4470. 줄번호/줄번호.c
@@ -3,14 +3,13 @@
 int main(void)
 {
 	int N;
+	char str[52]={0};
 
 	scanf("%d", &N);
 	getchar();
 
 	for(int i=1;i<=N;++i)
 	{
-		char str[52]={0};
-
 		fgets(str,sizeof(str),stdin);
 		printf("%d. %s", i, str);
 	}
