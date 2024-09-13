fe82813397db6f508a479d05918bf547f746d65d
WuHyun Park <0x847769a@gmail.com>
2024-09-13 21:48:47 +0900

    [Bronze II] Title: 단어의 개수, Time: 8 ms, Memory: 1968 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.c
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.c
@@ -1,22 +1,21 @@
 #include<stdio.h>
-#include<string.h>
-#include<ctype.h>
+
+int isalpha(char c)
+{
+	return 'a'<=c && c<='z' || 'A'<=c && c<='Z';
+}
 
 int main(void)
 {
-	char str[1000001];
-	int count=0;
+	char str[1000001]={0};
+	int count;
 
 	fgets(str,sizeof(str),stdin);
 
-	for(int i=0;i<strlen(str);i++)
-		if(i==0&&isalpha(str[i]))
-			count++;
-		else if(i==0)
-			continue;
-		else if(!isalpha(str[i-1])&&isalpha(str[i]))
-			count++;
+	count=isalpha(str[0]);
+	for(int i=1;str[i];++i)
+		count+=isalpha(str[i])&&!isalpha(str[i-1]);
 
-	printf("%d\n", count);
+	printf("%d", count);
 	return 0;
 }
