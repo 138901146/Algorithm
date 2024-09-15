5ee337cdc3a4a5fdbb635dec800d4499e606b65e
WuHyun Park <0x847769a@gmail.com>
2024-09-16 00:52:19 +0900

    [Platinum IV] Title: 축사 배정, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Platinum/2188. 축사 배정/축사 배정.c
+++ 백준/Platinum/2188. 축사 배정/축사 배정.c
@@ -1,23 +1,28 @@
 #include<stdio.h>
 #include<malloc.h>
 #include<stdbool.h>
+#include<memory.h>
 
 int *S=NULL, **cow=NULL, *cow_count=NULL;
 bool *visited=NULL;
 
 bool dfs(int current)
 {
-	for(int i=0;i<cow_count[current];i++)
-		if(!visited[cow[current][i]])
-		{
-			visited[cow[current][i]]=true;
+	for(int i=0;i<cow_count[current];++i)
+	{
+		int next=cow[current][i];
 
-			if(S[cow[current][i]]==0 || dfs(S[cow[current][i]]))
+		if(!visited[next])
+		{
+			visited[next]=true;
+
+			if(!S[next] || dfs(S[next]))
 			{
-				S[cow[current][i]]=current;
+				S[next]=current;
 				return true;
 			}
 		}
+	}
 
 	return false;
 }
@@ -32,24 +37,20 @@ int main(void)
 	S=(int *)calloc(M+1,sizeof(int));
 	visited=(bool *)malloc((M+1)*sizeof(bool));
 
-	for(int n=1;n<=N;n++)
+	for(int n=1;n<=N;++n)
 	{
 		scanf("%d", &cow_count[n]);
 		cow[n]=(int *)malloc(cow_count[n]*sizeof(int));
 
-		for(int i=0;i<cow_count[n];i++)
+		for(int i=0;i<cow_count[n];++i)
 			scanf("%d", &cow[n][i]);
 	}
 
-	for(int n=1;n<=N;n++)
+	for(int n=1;n<=N;++n)
 	{
-		for(int m=1;m<=M;m++)
-			visited[m]=false;
-		dfs(n);
+		memset(visited+1,0,M);
+		count+=dfs(n);
 	}
-
-	while(M)
-		count+=S[M--]>0;
 
 	printf("%d", count);
 	while(N)
