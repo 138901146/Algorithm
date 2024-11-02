3c5817c15a9962e7cf9f59822ef68f66dc0b77ea
WuHyun Park <0x847769a@gmail.com>
2024-11-02 12:08:01 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 0 ms, Memory: 1128 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.c
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.c
@@ -3,19 +3,19 @@
 
 int main(void)
 {
-	int x, y, root=0, T;
+	int x, y, root, T;
 
 	scanf("%d", &T);
 
-	for(int t=0;t<T;t++)
+	while(T--)
 	{
 		scanf("%d%d", &x, &y);
 
 		y-=x;
-		x=0;
-		root=(int)sqrt(y-1);
+		root=sqrt(y-1);
 
-		printf("%d\n", 2*root+(root*root+root<y));
+		printf("%d\n", (root<<1)+(root*root+root<y));
 	}
+
 	return 0;
 }
