3b224a557e2d0164ddbd5cb237df932c54272bb4
WuHyun Park <0x847769a@gmail.com>
2025-01-15 07:36:31 +0900

    [Bronze IV] Title: Last Factorial Digit, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/31048. Last Factorial Digit/Last Factorial Digit.c
+++ 백준/Bronze/31048. Last Factorial Digit/Last Factorial Digit.c
@@ -2,30 +2,14 @@
 
 int main(void)
 {
-	int T, N;
+	int T, N, ans[11]={1,1,2,6,4,0,};
 
 	scanf("%d", &T);
 
 	while(T--)
 	{
 		scanf("%d", &N);
-
-		switch(N)
-		{
-			case 0:
-				++N;
-			case 1:
-			case 2:
-			case 4:
-				printf("%d\n", N);
-				break;
-			case 3:
-				printf("6\n");
-				break;
-			default:
-				printf("0\n");
-				break;
-		}
+		printf("%d\n", ans[N]);
 	}
 
 	return 0;
