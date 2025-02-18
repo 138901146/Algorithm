353fd8127d8c611775172d5265441e38d04baeb0
WuHyun Park <0x847769a@gmail.com>
2025-02-18 23:06:08 +0900

    [Gold V] Title: 입국심사, Time: 68 ms, Memory: 1504 KB -BaekjoonHub

---
+++ 백준/Gold/3079. 입국심사/입국심사.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<malloc.h>
+
+#define INF 10000000000000000000
+
+int main(void)
+{
+	int N, M, *T=NULL;
+	unsigned long long left=1, right=INF, mid, ans=INF, count;
+
+	scanf("%d%d", &N, &M);
+	T=(int *)malloc(N*sizeof(int));
+
+	for(int k=0;k<N;++k)
+		scanf("%d", &T[k]);
+
+	while(left<=right)
+	{
+		count=0;
+		mid=(left+right)/2;
+
+		for(int k=0;k<N;++k)
+			count+=(unsigned long long)mid/T[k];
+
+		if(M<=count)
+		{
+			ans=mid;
+			right=mid-1;
+		}
+		else
+			left=mid+1;
+	}
+
+	printf("%llu", ans);
+	free(T);
+	return 0;
+}
