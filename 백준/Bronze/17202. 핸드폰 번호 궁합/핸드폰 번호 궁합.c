68da7d26ff2f650a6b1a25dbb84de8d6eb318c66
WuHyun Park <0x847769a@gmail.com>
2025-03-16 17:46:14 +0900

    [Bronze I] Title: 핸드폰 번호 궁합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/17202. 핸드폰 번호 궁합/핸드폰 번호 궁합.c
+++ 백준/Bronze/17202. 핸드폰 번호 궁합/핸드폰 번호 궁합.c
@@ -4,10 +4,10 @@ int main(void)
 {
 	int number[16][2], current=0, next=1, total=16;
 
-	for(int i=0;i<8;++i)
-		scanf("%1d", &number[i<<1][0]);
-	for(int i=0;i<8;++i)
-		scanf("%1d", &number[i<<1|1][0]);
+	for(int i=0;i<16;i+=2)
+		scanf("%1d", &number[i][0]);
+	for(int i=1;i<16;i+=2)
+		scanf("%1d", &number[i][0]);
 
 	while(2<total)
 	{
