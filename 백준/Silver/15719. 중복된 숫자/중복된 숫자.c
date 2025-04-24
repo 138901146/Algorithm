0b0b679be3e544bb4b99535cf1e659c01e5fe61d
WuHyun Park <0x847769a@gmail.com>
2025-04-25 08:17:23 +0900

    [Silver V] Title: 중복된 숫자, Time: 1692 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/15719. 중복된 숫자/중복된 숫자.c
+++ 백준/Silver/15719. 중복된 숫자/중복된 숫자.c
@@ -2,17 +2,17 @@
 
 int main(void)
 {
-	int N, M;
-	long long A=0;
+	long long N, M, A;
 
-	scanf("%d", &N);
+	scanf("%lld", &N);
+	A=-N*(N-1)>>1;
 
-	for(int i=0;i<N;++i)
+	while(N--)
 	{
-		scanf("%d", &M);
+		scanf("%lld", &M);
 		A+=M;
 	}
 
-	printf("%lld", A-((long long)N*(N-1)>>1));
+	printf("%lld", A);
 	return 0;
 }
