5370658bb778f176f2eb51f7cc5a1cff1a7c06de
WuHyun Park <0x847769a@gmail.com>
2025-07-17 07:37:01 +0900

    [Bronze IV] Title: Archivist, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int y;
+
+	scanf("%d", &y);
+
+	switch(y)
+	{
+		case 1996:
+		case 1997:
+		case 2000:
+		case 2007:
+		case 2008:
+		case 2013:
+		case 2018:
+			printf("SPbSU");
+			break;
+		case 2006:
+			printf("PetrSU, ITMO");
+			break;
+		default:
+			printf("ITMO");
+			break;
+	}
+
+	return 0;
+}
