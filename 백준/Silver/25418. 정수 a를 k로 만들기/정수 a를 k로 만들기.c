37914581221d4a9033bc17a04b32755887b8da39
WuHyun Park <0x847769a@gmail.com>
2025-08-02 06:18:52 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 8 ms, Memory: 8924 KB -BaekjoonHub

---
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int min[1000001]={0}, queue[1000000];
+
+int main(void)
+{
+	int A, K, front=0, rear=1;
+
+	scanf("%d%d", &A, &K);
+
+	min[A]=1;
+	queue[0]=A;
+
+	while(!min[K])
+	{
+		if(queue[front]+1<=K&&!min[queue[front]+1])
+		{
+			min[queue[front]+1]=min[queue[front]]+1;
+			queue[rear++]=queue[front]+1;
+		}
+
+		if((queue[front]<<1)<=K&&!min[queue[front]<<1])
+		{
+			min[queue[front]<<1]=min[queue[front]]+1;
+			queue[rear++]=queue[front]<<1;
+		}
+
+		++front;
+	}
+
+	printf("%d", min[K]-1);
+	return 0;
+}
