ed22f57a7df3b7dcc146942debfddf01f2691913
WuHyun Park <0x847769a@gmail.com>
2025-08-15 09:55:23 +0900

    [Silver III] Title: 발머의 피크 이론, Time: 112 ms, Memory: 9004 KB -BaekjoonHub

---
+++ 백준/Silver/27496. 발머의 피크 이론/발머의 피크 이론.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<malloc.h>
+
+int main(void)
+{
+	int N, L, a, count=0;
+	long long *C=NULL;
+
+	scanf("%d%d", &N, &L);
+	C=(long long *)calloc(N+L+1,sizeof(long long));
+
+	for(int i=0;i<N;++i)
+	{
+		scanf("%d", &a);
+		C[i+1]+=a;
+		C[i+1+L]-=a;
+	}
+
+	for(int i=1;i<=N;++i)
+	{
+		C[i]+=C[i-1];
+		count+=128<C[i]&&C[i]<139;
+	}
+
+	printf("%d", count);
+	free(C);
+	return 0;
+}
