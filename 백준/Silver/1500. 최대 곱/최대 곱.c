013834be2d1d8a7cc39e06903c6a1f0e48a10a63
WuHyun Park <0x847769a@gmail.com>
2025-08-17 00:00:09 +0900

    [Silver II] Title: 최대 곱, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1500. 최대 곱/최대 곱.c
+++ 백준/Silver/1500. 최대 곱/최대 곱.c
@@ -7,10 +7,8 @@ int main(void)
 	scanf("%lld%lld", &S, &K);
 
 	mod=S%K;
-	for(int i=0;i<mod;++i)
-		ans*=S/K+1;
-	for(int i=mod;i<K;++i)
-		ans*=S/K;
+	for(int i=0;i<K;++i)
+		ans*=S/K+(i<mod);
 
 	printf("%llu", ans);
 	return 0;
