27b6ab5eb89f12b72fc152c9983218cddaf1689b
WuHyun Park <0x847769a@gmail.com>
2025-08-17 09:06:12 +0900

    [Silver III] Title: NBA 농구, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.c
+++ 백준/Silver/2852. NBA 농구/NBA 농구.c
@@ -1,47 +1,32 @@
 #include<stdio.h>
-#include<stdlib.h>
-
-typedef struct
-{
-	int team, second;
-}
-status;
-
-int compare(const void *x,const void *y)
-{
-	return ((status *)x)->second<((status *)y)->second?-1:1;
-}
 
 int main(void)
 {
-	int N, m, s, winning[2]={0}, score[2]={0};
-	status goal[100];
+	int N, m, s, team[100], sec[100], win[2]={0}, score[2]={0};
 
 	scanf("%d", &N);
 
 	for(int n=0;n<N;++n)
 	{
-		scanf("%d%02d:%02d", &goal[n].team, &m, &s);
-		goal[n].second=60*m+s;
+		scanf("%d%02d:%02d", &team[n], &m, &s);
+		sec[n]=60*m+s;
 	}
 
-	qsort((void *)goal,(size_t)N,sizeof(status),compare);
-
-	++score[goal[0].team-1];
+	++score[team[0]-1];
 	for(int n=1;n<N;++n)
 	{
 		if(score[0]<score[1])
-			winning[1]+=goal[n].second-goal[n-1].second;
+			win[1]+=sec[n]-sec[n-1];
 		else if(score[1]<score[0])
-			winning[0]+=goal[n].second-goal[n-1].second;
-		++score[goal[n].team-1];
+			win[0]+=sec[n]-sec[n-1];
+		++score[team[n]-1];
 	}
 
 	if(score[0]<score[1])
-		winning[1]+=2880-goal[N-1].second;
+		win[1]+=2880-sec[N-1];
 	else if(score[1]<score[0])
-		winning[0]+=2880-goal[N-1].second;
+		win[0]+=2880-sec[N-1];
 
-	printf("%02d:%02d\n%02d:%02d", winning[0]/60, winning[0]%60, winning[1]/60, winning[1]%60);
+	printf("%02d:%02d\n%02d:%02d", win[0]/60, win[0]%60, win[1]/60, win[1]%60);
 	return 0;
 }
