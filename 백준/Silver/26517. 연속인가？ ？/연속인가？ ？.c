b06a4a89ad1e1338bc1d61d238b0627cf5111e02
WuHyun Park <0x847769a@gmail.com>
2025-08-20 06:55:54 +0900

    [Silver V] Title: 연속인가? ?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/26517. 연속인가？ ？/연속인가？ ？.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void)
+{
+	long long k, a, b, c, d;
+
+	scanf("%lld", &k);
+	scanf("%lld%lld%lld%lld", &a, &b, &c, &d);
+
+	if(a*k+b==c*k+d)
+		printf("Yes %lld", a*k+b);
+	else
+		printf("No");
+	return 0;
+}
