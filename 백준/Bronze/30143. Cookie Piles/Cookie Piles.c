fb7e75e3e3b52261e32b568d715cd7c88f190f7e
WuHyun Park <0x847769a@gmail.com>
2025-08-27 07:42:37 +0900

    [Bronze IV] Title: Cookie Piles, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/30143. Cookie Piles/Cookie Piles.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int T, N, A, D;
+
+	scanf("%d", &T);
+
+	while(T--)
+	{
+		scanf("%d%d%d", &N, &A, &D);
+		printf("%d\n", N*(2*A+(N-1)*D)>>1);
+	}
+
+	return 0;
+}
