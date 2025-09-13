538b72035f5c5af94f96d2b2441c5fdc13b8158c
WuHyun Park <0x847769a@gmail.com>
2025-09-14 08:13:56 +0900

    [Silver II] Title: 그르다 김가놈, Time: 620 ms, Memory: 4764 KB -BaekjoonHub

---
+++ 백준/Silver/18113. 그르다 김가놈/그르다 김가놈.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<malloc.h>
+
+int main(void)
+{
+	int N, K, M, *L=NULL;
+	long long left=1, right=9999999999999999, P=-1;
+
+	scanf("%d%d%d", &N, &K, &M);
+	L=(int *)malloc(N*sizeof(int));
+
+	for(int n=0;n<N;++n)
+		scanf("%d", &L[n]);
+
+	while(left<=right)
+	{
+		long long count=0, mid=left+right>>1;
+
+		for(int n=0;n<N;++n)
+		{
+			int cut=(K<<1)<=L[n]?L[n]-(K<<1):K<L[n]?L[n]-K:0;
+			count+=cut/mid;
+		}
+
+		if(count<M)
+			right=mid-1;
+		else
+		{
+			left=mid+1;
+			P=mid;
+		}
+	}
+
+	if(P==-1)
+	{
+		long long count=0;
+
+		for(int n=0;n<N;++n)
+		{
+			int cut=(K<<1)<=L[n]?L[n]-(K<<1):K<L[n]?L[n]-K:0;
+			count+=cut;
+		}
+
+		P=count<M?-1:1;
+	}
+
+	printf("%lld", P);
+	free(L);
+	return 0;
+}
