ecf0ec8da6c73e2d7d879fb050b623d0203317fd
WuHyun Park <0x847769a@gmail.com>
2025-11-16 07:17:42 +0900

    [Gold IV] Title: 나는 정말 휘파람을 못 불어, Time: 0 ms, Memory: 1308 KB -BaekjoonHub

--- 백준/Gold/24956. 나는 정말 휘파람을 못 불어/나는 정말 휘파람을 못 불어.c
+++ 백준/Gold/24956. 나는 정말 휘파람을 못 불어/나는 정말 휘파람을 못 불어.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 #include<malloc.h>
 
+#define MOD 1000000007
+
 int main(void)
 {
 	int N;
-	long long whisper[4]={0, };
+	long long whisper[4]={0};
 	char *S=NULL;
 
 	scanf("%d", &N);
@@ -14,16 +16,16 @@ int main(void)
 
 	for(int n=0;n<N;n++)
 		if(S[n]=='W')
-			whisper[0]=(whisper[0]+1)%1000000007;
+			whisper[0]=(whisper[0]+1)%MOD;
 		else if(S[n]=='H')
-			whisper[1]=(whisper[0]+whisper[1])%1000000007;
+			whisper[1]=(whisper[0]+whisper[1])%MOD;
 		else if(S[n]=='E')
 		{
-			whisper[3]=(whisper[2]+2*whisper[3])%1000000007;
-			whisper[2]=(whisper[1]+whisper[2])%1000000007;
+			whisper[3]=(whisper[2]+2*whisper[3])%MOD;
+			whisper[2]=(whisper[1]+whisper[2])%MOD;
 		}
 
-	printf("%lld\n", whisper[3]);
+	printf("%lld", whisper[3]);
 	free(S);
 	return 0;
 }
