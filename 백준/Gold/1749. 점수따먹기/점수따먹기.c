2c6884b2981c554ee2fc807ab84a05faf2594080
WuHyun Park <0x847769a@gmail.com>
2025-11-16 07:29:15 +0900

    [Gold IV] Title: 점수따먹기, Time: 272 ms, Memory: 1248 KB -BaekjoonHub

--- 백준/Gold/1749. 점수따먹기/점수따먹기.c
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<malloc.h>
 
 int main(void)
 {
@@ -7,29 +7,29 @@ int main(void)
 
 	scanf("%d%d", &N, &M);
 	matrix=(int **)malloc((N+1)*sizeof(int *));
-	for(int i=0;i<=N;i++)
+	for(int i=0;i<=N;++i)
 		matrix[i]=(int *)calloc(M+1,sizeof(int));
 
-	for(int i=1;i<=N;i++)
-		for(int j=1;j<=M;j++)
+	for(int i=1;i<=N;++i)
+		for(int j=1;j<=M;++j)
 			scanf("%d", &matrix[i][j]);
 
-	for(int i=0;i<=N;i++)
-		for(int j=1;j<=M;j++)
+	for(int i=0;i<=N;++i)
+		for(int j=1;j<=M;++j)
 			matrix[i][j]+=matrix[i][j-1];
 
-	for(int j=0;j<=M;j++)
-		for(int i=1;i<=N;i++)
+	for(int j=0;j<=M;++j)
+		for(int i=1;i<=N;++i)
 			matrix[i][j]+=matrix[i-1][j];
 
-	for(int i=1;i<=N;i++)
-		for(int j=1;j<=M;j++)
+	for(int i=1;i<=N;++i)
+		for(int j=1;j<=M;++j)
 			for(int k=0;k<i;k++)
 				for(int l=0;l<j;l++)
 					max=matrix[i][j]-matrix[k][j]-matrix[i][l]+matrix[k][l]>max?matrix[i][j]-matrix[k][j]-matrix[i][l]+matrix[k][l]:max;
 
 	printf("%d\n", max);
-	for(int i=0;i<=N;i++)
+	for(int i=0;i<=N;++i)
 		free(matrix[i]);
 	free(matrix);
 	return 0;
