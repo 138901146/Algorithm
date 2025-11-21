7b5f1ec3def1829bccace859fc8bd482f922a1aa
WuHyun Park <0x847769a@gmail.com>
2025-11-22 08:14:17 +0900

    [Gold V] Title: 팀원 모집, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Gold/11578. 팀원 모집/팀원 모집.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int N, M, O, P, solvable[10]={0}, max=11;
+
+	scanf("%d%d", &N, &M);
+
+	for(int i=0;i<M;++i)
+	{
+		scanf("%d", &O);
+		while(O--)
+		{
+			scanf("%d", &P);
+			solvable[i]|=1<<P-1;
+		}
+	}
+
+	for(int i=1;i<(1<<M);++i)
+	{
+		int solved=0, count=0;
+
+		for(int j=0;j<M;++j)
+			if((1<<j)&i)
+			{
+				++count;
+				solved|=solvable[j];
+			}
+
+		if(solved==(1<<N)-1)
+			max=count<max?count:max;
+	}
+
+	printf("%d", max==11?-1:max);
+	return 0;
+}
