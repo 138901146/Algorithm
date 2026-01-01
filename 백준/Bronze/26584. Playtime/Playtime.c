82ed924ab45d6ce53c34de8cf2ed8fb021c8d48f
WuHyun Park <0x847769a@gmail.com>
2026-01-01 23:47:08 +0900

    [Bronze I] Title: Playtime, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/26584. Playtime/Playtime.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+	int games;
+
+	scanf("%d", &games);
+	getchar();
+
+	while(games--)
+	{
+		char str[100]={0}, i;
+		int minute=0;
+
+		fgets(str,sizeof(str),stdin);
+
+		for(i=0;str[i]!=',';++i);
+		++i;
+
+		while('0'<=str[i]&&str[i]<='9')
+		{
+			minute*=10;
+			minute+=str[i++]-'0';
+		}
+
+		for(i=0;str[i]!=',';++i)
+			printf("%c", str[i]);
+		printf(" -");
+		if(525600<=minute)
+			printf(" %d year(s)", minute/525600);
+		minute%=525600;
+		if(1440<=minute)
+			printf(" %d day(s)", minute/1440);
+		minute%=1440;
+		if(60<=minute)
+			printf(" %d hour(s)", minute/60);
+		minute%=60;
+		if(minute)
+			printf(" %d minute(s)", minute);
+		printf("\n");
+	}
+
+	return 0;
+}
