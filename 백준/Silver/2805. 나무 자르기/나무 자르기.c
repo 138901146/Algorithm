f3918d13180991ba18e98fceaf33dbfee71d08c0
WuHyun Park <0x847769a@gmail.com>
2026-01-07 20:22:38 +0900

    [Silver II] Title: 나무 자르기, Time: 200 ms, Memory: 5020 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.c
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.c
@@ -3,34 +3,32 @@
 
 int main(void)
 {
-	int N, M, left=0, right=1000000000, mid, ans=0, *tree=NULL;
+	int N, *height=0;
+	long long M, left=0, right=2100000000, H=0;
 
-	scanf("%d%d", &N, &M);
-	tree=(int *)malloc(N*sizeof(int));
+	scanf("%d%lld", &N, &M);
+	height=(int *)malloc(N*sizeof(int));
 
-	for(int n=0;n<N;++n)
-		scanf("%d", &tree[n]);
+	for(int i=0;i<N;++i)
+		scanf("%d", &height[i]);
 
 	while(left<=right)
 	{
-		long long sum=0;
+		long long length=0, mid=left+right>>1;
 
-		mid=left+right>>1;
+		for(int i=0;i<N;++i)
+			length+=height[i]<mid?0:height[i]-mid;
 
-		for(int n=0;n<N;++n)
-			if(mid<tree[n])
-				sum+=tree[n]-mid;
-
-		if(M<=sum)
-		{
-			ans=mid;
-			left=mid+1;
-		}
-		else
+		if(length<M)
 			right=mid-1;
+		else
+		{
+			left=mid+1;
+			H=mid;
+		}
 	}
 
-	printf("%d", ans);
-	free(tree);
+	printf("%lld", H);
+	free(height);
 	return 0;
 }
