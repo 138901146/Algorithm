0976173349139cc29cbd48c2fee990d84057af06
WuHyun Park <0x847769a@gmail.com>
2026-01-28 06:42:37 +0900

    [Gold IV] Title: 정복자, Time: 16 ms, Memory: 1820 KB -BaekjoonHub

---
+++ 백준/Gold/14950. 정복자/정복자.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct
+{
+	int A, B, C;
+}
+edge;
+
+int compare(const void *x,const void *y)
+{
+	return ((edge *)x)->C<((edge *)y)->C?-1:1;
+}
+
+int *parent=NULL;
+
+int find_parent(int x)
+{
+	if(parent[x]==x)
+		return x;
+
+	return parent[x]=find_parent(parent[x]);
+}
+
+int union_parent(int x,int y)
+{
+	int parent_x=find_parent(x), parent_y=find_parent(y);
+
+	if(parent_x==parent_y)
+		return 0;
+
+	parent[parent_y]=parent_x;
+
+	return 1;
+}
+
+int main(void)
+{
+	int N, M, t, count=0;
+	long long total=0;
+	edge *road=NULL;
+
+	scanf("%d%d%d", &N, &M, &t);
+
+	road=(edge *)malloc(M*sizeof(edge));
+
+	for(int i=0;i<M;++i)
+		scanf("%d%d%d", &road[i].A, &road[i].B, &road[i].C);
+
+	qsort((void *)road,(size_t)M,sizeof(edge),compare);
+
+	parent=(int *)calloc(N+1,sizeof(int));
+	for(int i=1;i<=N;++i)
+		parent[i]=i;
+
+	for(int i=0;i<M&&count<N-1;++i)
+		if(union_parent(road[i].A,road[i].B))
+			total+=(long long)count++*t+road[i].C;
+
+	printf("%lld", total);
+	free(road);
+	free(parent);
+	return 0;
+}
